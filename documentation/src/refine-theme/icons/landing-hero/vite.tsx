61667b57b85be50d0998be980949abc409bf9c78
Stefan Pejcic <stefan@pejcic.rs>
2024-02-06 11:26:04 +0100

    Update vite.tsx

--- documentation/src/refine-theme/icons/landing-hero/vite.tsx
+++ documentation/src/refine-theme/icons/landing-hero/vite.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export const LandingHeroViteIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
-        width={64}
-        height={64}
+        width={24}
+        height={24}
         viewBox="0 0 213.000000 215.000000"
+        style="vertical-align:middle;"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
