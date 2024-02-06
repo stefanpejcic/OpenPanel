51fd035e411387da391c7a9d1ec6ad41e021d867
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-06 13:46:44 +0100

    Update auth0.tsx

--- documentation/src/refine-theme/icons/landing-hero/auth0.tsx
+++ documentation/src/refine-theme/icons/landing-hero/auth0.tsx
@@ -2,49 +2,14 @@ import React from "react";
 
 export const LandingHeroAuth0Icon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
-        width={64}
+        width={24}
         height={64}
-        viewBox="0 0 64 64"
+        viewBox="0 0 512 512"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-        <g
-            opacity={0.5}
-            className="hidden dark:block"
-            filter="url(#filter0_f_1305_2850)"
-        >
-            <path
-                d="M38.1744 39.415L35.8165 32L41.9863 27.4181H34.3565L31.9986 20H39.6289L41.9893 27.4175C43.356 31.7182 41.9462 36.6122 38.163 39.4138L38.1744 39.415ZM25.8311 39.415L32.001 44L38.1744 39.415L32.0045 34.8332L25.8311 39.415ZM22.0168 27.415C20.5742 31.9524 22.2488 36.758 25.8293 39.4169V39.415L28.1873 32L22.0174 27.415L29.646 27.4175L31.9986 20H24.364L22.0168 27.415Z"
-                fill="#EB5424"
-            />
-        </g>
-        <path
-            d="M38.1744 39.415L35.8165 32L41.9863 27.4181H34.3565L31.9986 20H39.6289L41.9893 27.4175C43.356 31.7182 41.9462 36.6122 38.163 39.4138L38.1744 39.415ZM25.8311 39.415L32.001 44L38.1744 39.415L32.0045 34.8332L25.8311 39.415ZM22.0168 27.415C20.5742 31.9524 22.2488 36.758 25.8293 39.4169V39.415L28.1873 32L22.0174 27.415L29.646 27.4175L31.9986 20H24.364L22.0168 27.415Z"
-            fill="#EB5424"
-        />
-        <defs>
-            <filter
-                id="filter0_f_1305_2850"
-                x={13.5}
-                y={12}
-                width={37}
-                height={40}
-                filterUnits="userSpaceOnUse"
-                colorInterpolationFilters="sRGB"
-            >
-                <feFlood floodOpacity={0} result="BackgroundImageFix" />
-                <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="BackgroundImageFix"
-                    result="shape"
-                />
-                <feGaussianBlur
-                    stdDeviation={4}
-                    result="effect1_foregroundBlur_1305_2850"
-                />
-            </filter>
-        </defs>
+    <path stroke="#066da5" stroke-width="38" d="M296 226h42m-92 0h42m-91 0h42m-91 0h41m-91 0h42m8-46h41m8 0h42m7 0h42m-42-46h42" />
+    <path fill="#066da5" d="m472 228s-18-17-55-11c-4-29-35-46-35-46s-29 35-8 74c-6 3-16 7-31 7H68c-5 19-5 145 133 145 99 0 173-46 208-130 52 4 63-39 63-39" />
     </svg>
 );
