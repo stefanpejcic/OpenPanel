5d72bca334df89e0aeed27159624704e0fe380a8
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-06 13:04:49 +0100

    Update chakra.tsx

--- documentation/src/refine-theme/icons/landing-hero/chakra.tsx
+++ documentation/src/refine-theme/icons/landing-hero/chakra.tsx
@@ -4,77 +4,30 @@ export const LandingHeroChakraUIIcon = (
     props: React.SVGProps<SVGSVGElement>,
 ) => (
     <svg
-        width={64}
+        width={24}
         height={64}
-        viewBox="0 0 64 64"
+        viewBox="0 0 256 255"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-        <g
-            opacity={0.5}
-            className="hidden dark:block"
-            filter="url(#filter0_f_1305_2683)"
-        >
-            <circle cx={32} cy={32} r={11.25} fill="white" />
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M32 20C38.6274 20 44 25.3726 44 32C44 38.6274 38.6274 44 32 44C25.3726 44 20 38.6274 20 32C20 25.3726 25.3726 20 32 20ZM34.6755 24.3888L26.4957 32.5123C26.3774 32.6298 26.4606 32.8316 26.6273 32.8316H32.1941C32.3466 32.8316 32.4347 33.0045 32.3453 33.128L27.9809 39.1488C27.8481 39.3318 28.0942 39.5492 28.2595 39.395L37.4792 30.7903C37.6031 30.6746 37.5213 30.467 37.3518 30.467H32.0905C31.9488 30.467 31.8586 30.3151 31.9266 30.1907L34.9711 24.6108C35.0743 24.4217 34.8283 24.237 34.6755 24.3888Z"
-                fill="url(#paint0_linear_1305_2683)"
-            />
-        </g>
-        <circle cx={32} cy={32} r={11.25} fill="white" />
-        <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M32 20C38.6274 20 44 25.3726 44 32C44 38.6274 38.6274 44 32 44C25.3726 44 20 38.6274 20 32C20 25.3726 25.3726 20 32 20ZM34.6755 24.3888L26.4957 32.5123C26.3774 32.6298 26.4606 32.8316 26.6273 32.8316H32.1941C32.3466 32.8316 32.4347 33.0045 32.3453 33.128L27.9809 39.1488C27.8481 39.3318 28.0942 39.5492 28.2595 39.395L37.4792 30.7903C37.6031 30.6746 37.5213 30.467 37.3518 30.467H32.0905C31.9488 30.467 31.8586 30.3151 31.9266 30.1907L34.9711 24.6108C35.0743 24.4217 34.8283 24.237 34.6755 24.3888Z"
-            fill="url(#paint1_linear_1305_2683)"
-        />
         <defs>
-            <filter
-                id="filter0_f_1305_2683"
-                x={12}
-                y={12}
-                width={40}
-                height={40}
-                filterUnits="userSpaceOnUse"
-                colorInterpolationFilters="sRGB"
-            >
-                <feFlood floodOpacity={0} result="BackgroundImageFix" />
-                <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="BackgroundImageFix"
-                    result="shape"
-                />
-                <feGaussianBlur
-                    stdDeviation={4}
-                    result="effect1_foregroundBlur_1305_2683"
-                />
-            </filter>
-            <linearGradient
-                id="paint0_linear_1305_2683"
-                x1={32}
-                y1={20}
-                x2={32}
-                y2={44}
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop stopColor="#7BCBD4" />
-                <stop offset={1} stopColor="#29C6B7" />
-            </linearGradient>
-            <linearGradient
-                id="paint1_linear_1305_2683"
-                x1={32}
-                y1={20}
-                x2={32}
-                y2={44}
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop stopColor="#7BCBD4" />
-                <stop offset={1} stopColor="#29C6B7" />
-            </linearGradient>
-        </defs>
+        <linearGradient x1="12.959%" y1="12.039%" x2="79.639%" y2="78.201%" id="a">
+            <stop stop-color="#387EB8" offset="0%" />
+            <stop stop-color="#366994" offset="100%" />
+        </linearGradient>
+        <linearGradient x1="19.128%" y1="20.579%" x2="90.742%" y2="88.429%" id="b">
+            <stop stop-color="#FFE052" offset="0%" />
+            <stop stop-color="#FFC331" offset="100%" />
+        </linearGradient>
+    </defs>
+    <path
+        d="M126.916.072c-64.832 0-60.784 28.115-60.784 28.115l.072 29.128h61.868v8.745H41.631S.145 61.355.145 126.77c0 65.417 36.21 63.097 36.21 63.097h21.61v-30.356s-1.165-36.21 35.632-36.21h61.362s34.475.557 34.475-33.319V33.97S194.67.072 126.916.072zM92.802 19.66a11.12 11.12 0 0 1 11.13 11.13 11.12 11.12 0 0 1-11.13 11.13 11.12 11.12 0 0 1-11.13-11.13 11.12 11.12 0 0 1 11.13-11.13z"
+        fill="url(#a)"
+    />
+    <path
+        d="M128.757 254.126c64.832 0 60.784-28.115 60.784-28.115l-.072-29.127H127.6v-8.745h86.441s41.486 4.705 41.486-60.712c0-65.416-36.21-63.096-36.21-63.096h-21.61v30.355s1.165 36.21-35.632 36.21h-61.362s-34.475-.557-34.475 33.32v56.013s-5.235 33.897 62.518 33.897zm34.114-19.586a11.12 11.12 0 0 1-11.13-11.13 11.12 11.12 0 0 1 11.13-11.131 11.12 11.12 0 0 1 11.13 11.13 11.12 11.12 0 0 1-11.13 11.13z"
+        fill="url(#b)"
+    />
     </svg>
 );
