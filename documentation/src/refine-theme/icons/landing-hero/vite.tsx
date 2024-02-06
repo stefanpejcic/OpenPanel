eb3043a9f5f8fdb85c63ef241d2cc3ff14c39aab
Stefan Pejcic <stefan@pejcic.rs>
2024-02-06 11:55:53 +0100

    Update vite.tsx

--- documentation/src/refine-theme/icons/landing-hero/vite.tsx
+++ documentation/src/refine-theme/icons/landing-hero/vite.tsx
@@ -5,7 +5,6 @@ export const LandingHeroViteIcon = (props: React.SVGProps<SVGSVGElement>) => (
         width={64}
         height={64}
         viewBox="0 0 213.000000 215.000000"
-        style="vertical-align:middle;"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
@@ -15,5 +14,28 @@ export const LandingHeroViteIcon = (props: React.SVGProps<SVGSVGElement>) => (
             d="M990 2071 c-39 -13 -141 -66 -248 -129 -53 -32 -176 -103 -272 -158 -206 -117 -276 -177 -306 -264 -17 -50 -19 -88 -19 -460 0 -476 0 -474 94 -568 55 -56 124 -98 604 -369 169 -95 256 -104 384 -37 104 54 532 303 608 353 76 50 126 113 147 184 8 30 12 160 12 447 0 395 -1 406 -22 461 -34 85 -98 138 -317 264 -104 59 -237 136 -295 170 -153 90 -194 107 -275 111 -38 2 -81 0 -95 -5z m205 -561 c66 -38 166 -95 223 -127 l102 -58 0 -262 c0 -262 0 -263 -22 -276 -13 -8 -52 -31 -88 -51 -36 -21 -126 -72 -200 -115 l-135 -78 -3 261 -3 261 -166 95 c-91 52 -190 109 -219 125 -30 17 -52 34 -51 39 3 9 424 256 437 255 3 0 59 -31 125 -69z"
         ></path>
     </g>
-</svg>
+        <defs>
+            <filter
+                id="filter0_f_1305_2599"
+                x={13.5}
+                y={12}
+                width={37}
+                height={40}
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+            >
+                <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+                <feGaussianBlur
+                    stdDeviation={4}
+                    result="effect1_foregroundBlur_1305_2599"
+                />
+            </filter>
+        </defs>
+    </svg>
 );
