b4473403519326a82a229cf6147c07f56d07a88d
Stefan Pejcic <stefan@pejcic.rs>
2024-02-09 23:45:24 +0100

    Update top-announcement.tsx

--- documentation/src/refine-theme/top-announcement.tsx
+++ documentation/src/refine-theme/top-announcement.tsx
@@ -132,8 +132,8 @@ const Text = () => {
             )}
         >
             <div className={clsx("ml-2", "not-prose")}>
-                <span className={clsx("font-semibold")}>OpenPanel BETA</span>{" "}
-                is out! Give it a spin and tell us what you think
+                <span className={clsx("font-semibold")}>OpenPanel Community</span>{" "}
+                edition is out! Give it a spin and tell us what you think
                 <span
                     className={clsx(
                         "text-refine-cyan-alt hover:text-refine-cyan-alt",
