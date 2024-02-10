98900770e81af30f1332bff4510d1a5e70a59d2d
Stefan Pejcic <stefan@pejcic.rs>
2024-02-10 15:58:11 +0100

    features renamed

--- documentation/src/refine-theme/footer-data.tsx
+++ documentation/src/refine-theme/footer-data.tsx
@@ -41,7 +41,7 @@ export const menuItems = [
             },
             {
                 label: "Features",
-                href: "/integrations",
+                href: "/features",
             },
         ],
     },
