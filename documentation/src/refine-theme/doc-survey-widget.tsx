e889326257d5859dcce8df4198dd987d152be1ac
Stefan Pejcic <stefan@pejcic.rs>
2024-02-11 19:51:28 +0100

    Update doc-survey-widget.tsx

--- documentation/src/refine-theme/doc-survey-widget.tsx
+++ documentation/src/refine-theme/doc-survey-widget.tsx
@@ -60,7 +60,7 @@ export const DocSurveyWidget = ({ className }: Props) => {
         }
 
         const data = await updateSurvey({
-            surveyId: survey.id,
+            surveyId: survey?.id,
             body: { response: selectedOption, responseText: text },
         });
         if (!data) return;
