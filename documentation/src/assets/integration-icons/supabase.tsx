49cb2200d952576b11dfc197755742bd6fdfde7d
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-28 10:46:09 +0100

    Update supabase.tsx

--- documentation/src/assets/integration-icons/supabase.tsx
+++ documentation/src/assets/integration-icons/supabase.tsx
@@ -13,31 +13,7 @@ const SvgSupabase = (props: SVGProps<SVGSVGElement>) => (
     <path stroke="#066da5" stroke-width="38" d="M296 226h42m-92 0h42m-91 0h42m-91 0h41m-91 0h42m8-46h41m8 0h42m7 0h42m-42-46h42" />
     <path fill="#066da5" d="m472 228s-18-17-55-11c-4-29-35-46-35-46s-29 35-8 74c-6 3-16 7-31 7H68c-5 19-5 145 133 145 99 0 173-46 208-130 52 4 63-39 63-39" />
     </svg>
-        <defs>
-            <linearGradient
-                id={`${props?.id || ""}-supabase-a`}
-                x1="23.1353"
-                y1="23.4843"
-                x2="40.3698"
-                y2="30.7124"
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop stopColor="#249361" />
-                <stop offset="1" stopColor="#3ECF8E" />
-            </linearGradient>
-            <linearGradient
-                id={`${props?.id || ""}-supabase-b`}
-                x1="15.4944"
-                y1="13.0225"
-                x2="23.3542"
-                y2="27.8183"
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop />
-                <stop offset="1" stopOpacity="0" />
-            </linearGradient>
-        </defs>
-    </svg>
+       
 );
 
 export default SvgSupabase;
