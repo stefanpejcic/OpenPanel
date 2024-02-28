119712427a3545626cf37e75d01d95a51b18a996
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-28 11:50:31 +0100

    Create docker.tsx

---
+++ documentation/src/assets/integration-icons/docker.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const SvgDocker = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        width={24}
+        height={64}
+        viewBox="0 0 512 512"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+    <path stroke="#066da5" stroke-width="38" d="M296 226h42m-92 0h42m-91 0h42m-91 0h41m-91 0h42m8-46h41m8 0h42m7 0h42m-42-46h42" />
+    <path fill="#066da5" d="m472 228s-18-17-55-11c-4-29-35-46-35-46s-29 35-8 74c-6 3-16 7-31 7H68c-5 19-5 145 133 145 99 0 173-46 208-130 52 4 63-39 63-39" />
+    </svg>
+       
+);
+
+export default SvgDocker;
