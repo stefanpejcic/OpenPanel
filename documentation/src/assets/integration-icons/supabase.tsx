d40e6cd7cc86c169a929cccadc2dc74a1744b657
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-28 10:59:54 +0100

    Update supabase.tsx viweport

--- documentation/src/assets/integration-icons/supabase.tsx
+++ documentation/src/assets/integration-icons/supabase.tsx
@@ -5,7 +5,7 @@ const SvgSupabase = (props: SVGProps<SVGSVGElement>) => (
     <svg
         width={24}
         height={24}
-        viewBox="0 0 512 512"
+        viewBox="0 0 47 48"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
