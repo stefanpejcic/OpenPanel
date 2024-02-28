1c95fb7573f993505e8abbebd9f8bd983fdd56d1
Stefan Pejcic <stefan@pejcic.rs>
2024-02-28 21:37:06 +0100

    Update use-cases.js

--- documentation/src/assets/use-cases.js
+++ documentation/src/assets/use-cases.js
@@ -1,17 +1,17 @@
 /** @type {import('../types/use-cases').UseCases[]} */
 module.exports = [
     {
-        route: "usystem",
-        title: "QUOTE/PURCHASE INTERFACE",
+        route: "panel",
+        title: "OpenPanel Documentation",
         description:
-            "Usystem has developed a <strong>system for quoting/purchasing</strong> products for a logistics company in Paraguay via <strong>refine</strong>, the operator performs the purchase request for parts for vehicles, machines or administrative items.",
+            "Documentation and How-to tutorials for <strong>end-user interface</strong> where customers can add domains, manage websites and configure services.",
         thumbImgURL: "/use-cases/usystem.png",
     },
     {
-        route: "gravity",
-        title: "DASHBOARD",
+        route: "admin",
+        title: "OpenAdmin Documentation",
         description:
-            "Gravity Enterprises has created a <strong>dashboard</strong> from scratch via <strong>refine</strong>.",
+            "Documentation for the <strong>Administrator users</strong>.",
         thumbImgURL: "/use-cases/gravity.png",
     },
 ];
