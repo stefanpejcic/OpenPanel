cc547933c35bc1e0e16c6d3770c3a2ee4a95664e
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-28 17:46:27 +0100

    Update phpmyadmin.tsx

--- documentation/src/assets/integration-icons/phpmyadmin.tsx
+++ documentation/src/assets/integration-icons/phpmyadmin.tsx
@@ -2,40 +2,32 @@ import * as React from "react";
 import { SVGProps } from "react";
 
 const SvgPhpmyadmin = (props: SVGProps<SVGSVGElement>) => (
-<svg width="24px" height="24px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <!-- Define light mode colors -->
-  <defs>
-    <linearGradient id="lightGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-      <stop offset="0%" style="stop-color:#2F88FF;stop-opacity:1" />
-      <stop offset="100%" style="stop-color:#FFFFFF;stop-opacity:1" />
-    </linearGradient>
-    <linearGradient id="lightStroke" x1="0%" y1="0%" x2="100%" y2="100%">
-      <stop offset="0%" style="stop-color:#000000;stop-opacity:1" />
-      <stop offset="100%" style="stop-color:#FFFFFF;stop-opacity:1" />
-    </linearGradient>
-  </defs>
-
-  <!-- Define dark mode colors -->
-  <defs>
-    <linearGradient id="darkGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-      <stop offset="0%" style="stop-color:#FFFFFF;stop-opacity:1" />
-      <stop offset="100%" style="stop-color:#2F88FF;stop-opacity:1" />
-    </linearGradient>
-    <linearGradient id="darkStroke" x1="0%" y1="0%" x2="100%" y2="100%">
-      <stop offset="0%" style="stop-color:#FFFFFF;stop-opacity:1" />
-      <stop offset="100%" style="stop-color:#000000;stop-opacity:1" />
-    </linearGradient>
-  </defs>
-
-  <!-- Use the appropriate gradient based on dark mode -->
-  <rect width="24" height="24" fill="url(#lightGradient)" fill-opacity="0.01"/>
-  <path d="M34 12V20V21C31.0449 21 28.3892 22.2818 26.5585 24.3198C24.9678 26.0906 24 28.4323 24 31C24 31.5789 24.0492 32.1463 24.1436 32.6983C24.6579 35.7046 26.5143 38.2529 29.0741 39.7046C26.4116 40.5096 22.8776 41 19 41C10.7157 41 4 38.7614 4 36V28V20V12" stroke="url(#lightStroke)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M34 12C34 14.7614 27.2843 17 19 17C10.7157 17 4 14.7614 4 12C4 9.23858 10.7157 7 19 7C27.2843 7 34 9.23858 34 12Z" fill="url(#darkGradient)" stroke="url(#darkStroke)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M4 28C4 30.7614 10.7157 33 19 33C20.807 33 22.5393 32.8935 24.1436 32.6983" stroke="url(#lightStroke)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M4 20C4 22.7614 10.7157 25 19 25C21.7563 25 24.339 24.7522 26.5585 24.3198" stroke="url(#lightStroke)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M44 31C44 36.5228 39.5228 41 34 41C32.2091 41 30.5281 40.5292 29.0741 39.7046C26.5143 38.2529 24.6579 35.7046 24.1436 32.6983C24.0492 32.1463 24 31.5789 24 31C24 28.4323 24.9678 26.0906 26.5585 24.3198C28.3892 22.2818 31.0449 21 34 21C39.5228 21 44 25.4772 44 31Z" fill="url(#darkGradient)" stroke="url(#darkStroke)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M37 29L39 31L37 33" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M31 33L29 31L31 29" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+<svg fill="#000000" height="24px" width="24px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 58.201 58.201" stroke="currentColor" xml:space="preserve">
+<g>
+	<path d="M52.524,45.537c-0.429-4.107-3.852-7.332-7.953-7.332c-2.564,0-4.984,1.286-6.48,3.402
+		c-0.291-0.051-0.586-0.076-0.883-0.076c-2.32,0-4.381,1.577-5.066,3.813c-1.907,1.131-3.116,3.241-3.116,5.502
+		c0,3.507,2.785,6.359,6.208,6.359h10.774c0.06,0,0.118-0.004,0.171-0.008c0.062,0.004,0.124,0.008,0.188,0.008h4.31
+		c3.225,0,5.849-2.687,5.849-5.989C56.525,48.616,54.884,46.342,52.524,45.537z M50.677,55.205l-4.352-0.003l-0.17-0.006
+		l-10.922,0.009c-2.32,0-4.208-1.956-4.208-4.359c0-1.665,0.949-3.21,2.417-3.935l0.435-0.214l0.102-0.475
+		c0.331-1.56,1.689-2.691,3.229-2.691c0.345,0,0.688,0.057,1.02,0.168l0.777,0.261l0.408-0.711c1.096-1.906,3.024-3.044,5.158-3.044
+		c3.265,0,5.955,2.747,5.998,6.123l0.011,0.813l0.799,0.155c1.823,0.354,3.146,2.002,3.146,3.919
+		C54.525,53.415,52.799,55.205,50.677,55.205z"/>
+	<path d="M29.875,43.044c1.363-2.618,3.96-4.337,6.872-4.5c2.066-2.127,4.894-3.339,7.824-3.339c2.531,0,4.916,0.916,6.818,2.455
+		c0.095-0.087,0.203-0.173,0.287-0.26v-1.289v-0.5v-0.5V28c-1.318,0.905-3.028,1.697-5.025,2.367
+		c-4.21,1.472-10.048,2.502-17.369,2.703c-0.87,0.027-1.74,0.041-2.606,0.041c-0.869,0-1.742-0.014-2.614-0.042
+		c-7.341-0.201-13.191-1.238-17.403-2.717C4.68,29.685,2.984,28.899,1.676,28v7.111v0.5v0.5V37.4c2.846,2.971,12.394,5.711,25,5.711
+		C27.769,43.111,28.83,43.083,29.875,43.044z"/>
+	<path d="M24.311,29.965c0.326,0.009,0.651,0.018,0.982,0.023C25.749,29.996,26.21,30,26.676,30s0.926-0.004,1.383-0.011
+		c0.33-0.005,0.656-0.015,0.982-0.023c0.116-0.003,0.234-0.005,0.349-0.008c11.253-0.359,19.648-2.915,22.286-5.668V23v-0.5V22
+		v-7.111C46.808,18.232,36.52,20,26.676,20s-20.133-1.768-25-5.111V22v0.5V23v1.289c2.638,2.754,11.033,5.31,22.286,5.668
+		C24.077,29.96,24.195,29.962,24.311,29.965z"/>
+	<path d="M51.676,11.306V9.5V9c0-0.168-0.056-0.319-0.135-0.458C50.418,4.241,41.791,0,26.676,0C11.598,0,2.979,4.22,1.822,8.51
+		C1.737,8.657,1.676,8.818,1.676,9v0.5v1.806C4.512,14.267,13.992,17,26.676,17S48.839,14.267,51.676,11.306z"/>
+	<path d="M26.025,50.845c0-1.652,0.451-3.249,1.249-4.651c-0.201,0.001-0.395,0.007-0.599,0.007c-12.346,0-20.866-2.29-25-5.201
+		v8.201c0,0.162,0.043,0.315,0.117,0.451c1.181,4.895,11.747,8.549,24.883,8.549c0.95,0,1.881-0.024,2.801-0.062
+		C27.376,56.422,26.025,53.792,26.025,50.845z"/>
+</g>
 </svg>
 );
 
