e40c061cfb6567ca28138ab37ec97450f40ccc1e
Stefan Pejcic <stefan@pejcic.rs>
2024-02-28 22:09:08 +0100

    Update use-cases.js

--- documentation/src/assets/use-cases.js
+++ documentation/src/assets/use-cases.js
@@ -4,14 +4,14 @@ module.exports = [
         route: "panel",
         title: "OpenPanel Documentation",
         description:
-            "Documentation and How-to tutorials for <strong>end-user interface</strong> where customers can add domains, manage websites and configure services.",
+            "Guides and Tutorials for the <strong>End-User Interface</strong>, enabling customers to add domains, manage websites, and configure services.",
         thumbImgURL: "/img/panel/v1/dashboard/dashboard.png",
     },
     {
         route: "admin",
         title: "OpenAdmin Documentation",
         description:
-            "Documentation for the <strong>Administrator users</strong>.",
+            "Guidance for <strong>Administrator Users</strong> on user creation and plan setup, server management, and navigating the administrative interface.",
         thumbImgURL: "/img/admin/openadmin_dashboard.png",
     },
 ];
