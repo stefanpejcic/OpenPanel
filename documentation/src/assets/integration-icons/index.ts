22626473a639656ce3a9ce516894fbed2bd171b2
Filip Cumpalovic <152178979+f1lk3@users.noreply.github.com>
2024-02-28 12:28:39 +0100

    Update index.ts

--- documentation/src/assets/integration-icons/index.ts
+++ documentation/src/assets/integration-icons/index.ts
@@ -50,6 +50,7 @@ export { default as Redis } from "./redis";
 export { default as Docker } from "./docker";
 export { default as Named } from "./named";
 export { default as Ufw } from "./ufw";
+export { default as Srvnginx } from "./srvnginx";
 
 export { default as Ably } from "./ably";
 export { default as Airtable } from "./airtable";
