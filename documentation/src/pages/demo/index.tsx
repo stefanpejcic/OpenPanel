b91dd7563657eb71e926d9b35a16bc81b3a0a6c1
Stefan Pejcic <stefan@pejcic.rs>
2024-02-29 11:45:07 +0100

    Update index.tsx

--- documentation/src/pages/demo/index.tsx
+++ documentation/src/pages/demo/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "@docusaurus/Head";
+import Link from "@docusaurus/Link";
 import { BlogFooter } from "@site/src/refine-theme/blog-footer";
 import { CommonHeader } from "@site/src/refine-theme/common-header";
 import { CommonLayout } from "@site/src/refine-theme/common-layout";
@@ -15,8 +16,41 @@ const Demo: React.FC = () => {
             <div className="refine-prose">
                 <CommonHeader hasSticky={true} />
 
+                <div
+                    className={clsx(
+                        "not-prose",
+                        "xl:max-w-[944px] xl:py-16",
+                        "lg:max-w-[912px] lg:py-10",
+                        "md:max-w-[624px] md:text-4xl  md:pb-6 pt-6",
+                        "sm:max-w-[480px] text-xl",
+                        "max-w-[328px]",
+                        "w-full mx-auto",
+                    )}
+                >
+                    <h1
+                        className={clsx(
+                            "font-semibold",
+                            "!mb-0",
+                            "text-gray-900 dark:text-gray-0",
+                            "text-xl md:text-[40px] md:leading-[56px]",
+                        )}
+                    >
+                        Live Demo for{" "}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r text-gradient-to-r from-[#0FBDBD] to-[#26D97F]">
+                            OpenPanel
+                        </span>
+                        , and{" "}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r text-gradient-to-r from-[#FF9933] to-[#FF4C4D]">
+                            OpenAdmin
+                        </span>
+                        .
+                    </h1>
+                </div>
+
+
+
+                
                 <div className="flex-1 flex flex-col pt-8 lg:pt-16 pb-32 max-w-[800px] w-full mx-auto px-2">
-                    <h1>Live Demo</h1>
 <p>
 Explore OpenAdmin & OpenPanel® with no strings attached.
 </p>
