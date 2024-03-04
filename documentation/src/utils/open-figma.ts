6c3ae6f287aba4e4ab826fd14e2f680eb832450b
Stefan Pejcic <stefan@pejcic.rs>
2024-03-04 11:20:14 +0100

    assets

--- documentation/src/utils/open-figma.ts
+++ documentation/src/utils/open-figma.ts
@@ -1,5 +1,5 @@
 export const openFigma = () => {
     return window
-        .open("https://openpanel.co/contact", "_blank")
+        .open("https://openpanel.co/assets", "_blank")
         ?.focus();
 };
