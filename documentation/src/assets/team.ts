87270ff002c3c4b66a821d6bfea090a4e8692d88
Stefan Pejcic <stefan@pejcic.rs>
2024-04-23 19:14:44 +0200

    Update team.ts

--- documentation/src/assets/team.ts
+++ documentation/src/assets/team.ts
@@ -1,45 +1,24 @@
 export const team = [
     {
-        name: "Civan Özseyhan",
+        name: "Vladimir Duka",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/civan.png",
         role1: "Co-Founder",
         role2: "CEO",
     },
     {
-        name: "Ömer Aplak",
+        name: "Stefan Pejcic",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/omer.png",
         role1: "Co-Founder",
         role2: "CTO",
     },
     {
-        name: "Eren Erkalkan",
+        name: "Radovan Jecmenica",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/eren.png",
-        role1: "Co-Founder",
-        role2: "CPO",
-    },
-    {
-        name: "Batuhan Özgür Özdemir",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
-        role1: "Tech Lead",
-    },
-    {
-        name: "Ali Emir Şen",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/ali.png",
-        role1: "Senior Software Developer",
-    },
-    {
-        name: "Alican Erdurmaz",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/alican.png",
         role1: "Software Developer",
     },
     {
-        name: "Necati Ozmen",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/necati.png",
-        role1: "Head of Growth",
-    },
-    {
-        name: "Recep Kütük",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/recep.png",
-        role1: "Senior Product Designer",
+        name: "Petar Curic",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        role1: "Product Designer",
     },
 ];
