b3068cf1255c89cc527e793f9deca2baa30e2f92
Stefan Pejcic <stefan@pejcic.rs>
2024-04-23 18:46:45 +0200

    Create index.tsx

---
+++ documentation/src/components/install/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InstallSection: React.FC = () => {
+  return (
+    <div>
+      <h2>Installation Steps</h2>
+      <p>This is your installation guide...</p>
+    </div>
+  );
+};
+
+export default InstallSection;
