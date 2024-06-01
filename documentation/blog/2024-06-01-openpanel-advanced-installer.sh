45424fd8b6d0b048a46fa223b3791fc5f2ad7210
Stefan Pejcic <stefan@pejcic.rs>
2024-06-01 16:48:32 +0200

    Create 2024-06-01-openpanel-advanced-installer.sh

---
+++ documentation/blog/2024-06-01-openpanel-advanced-installer.sh
@@ -0,0 +1,15 @@
+---
+title: OpenPanel Advanced installed
+description: Create custom instlal command
+slug: openpanel-advanced-installer
+authors: stefanpejcic
+tags: [OpenPanel, news, install]
+image: https://openpanel.co/img/blog/advanced_installer.png
+hide_table_of_contents: true
+---
+
+https://openpanel.co/install page allows you to customize the OpenPanel installation process.
+
+<!--truncate-->
+
+https://openpanel.co/install 
