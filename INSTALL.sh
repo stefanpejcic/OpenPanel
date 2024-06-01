cd21d7827a614762335d25b6edb2885b3a89761f
Stefan Pejcic <stefan@pejcic.rs>
2024-06-01 12:56:37 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1049,7 +1049,11 @@ setup_opencli() {
 
    # added in 0.1.8
     mkdir -p /etc/openpanel/openadmin/config/
+    
     wget -O /etc/openpanel/openadmin/config/forbidden_usernames.txt  https://gist.githubusercontent.com/stefanpejcic/f08e6841fbf953b7aff108a8c154e9eb/raw/9ac3efabbde48faf95435221e7e09e28b46340ae/forbidden_usernames.ttxt > /dev/null 2>&1
+
+    # added in 0.2.0
+    touch /etc/openpanel/openadmin/config/terms
 
     echo "Creating directories for logs.."
     debug_log mkdir -p ${OPENPANEL_ERR_DIR}admin ${OPENPANEL_ERR_DIR}user
