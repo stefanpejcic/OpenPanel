e7fac6b6aed149b4e4d175f0eff145977ed61847
Stefan Pejcic <stefan@pejcic.rs>
2024-06-01 16:50:01 +0200

    Update common-footer.tsx

--- documentation/src/refine-theme/common-footer.tsx
+++ documentation/src/refine-theme/common-footer.tsx
@@ -27,7 +27,7 @@ export const CommonFooter = () => {
                         // "mt-4 sm:mt-0",
                     )}
                 >
-                    OpenPanel Co. © 2024
+                    OpenPanel © 2024
                 </div>
 
                 <div
