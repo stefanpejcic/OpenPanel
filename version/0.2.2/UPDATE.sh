6700c9c0bb44f15d22c1a7766961a961135b9a0d
Stefan Pejcic <stefan@pejcic.rs>
2024-07-16 17:03:43 +0200

    Update UPDATE.sh

--- version/0.2.2/UPDATE.sh
+++ version/0.2.2/UPDATE.sh
@@ -221,12 +221,16 @@ opencli_update(){
     echo "Updating OpenCLI commands from https://storage.googleapis.com/openpanel${NEW_PANEL_VERSION}/get.openpanel.co/downloads/${NEW_PANEL_VERSION}/opencli/opencli-main.tar.gz"
     echo ""
     mkdir -p ${TEMP_DIR}opencli
-    wget -O ${TEMP_DIR}opencli.tar.gz "https://storage.googleapis.com/openpanel/${version}/get.openpanel.co/downloads/${version}/opencli/opencli-main.tar.gz"
+    wget -O ${TEMP_DIR}opencli.tar.gz "https://storage.googleapis.com/openpanel/${NEW_PANEL_VERSION}/get.openpanel.co/downloads/${NEW_PANEL_VERSION}/opencli/opencli-main.tar.gz"
     cd ${TEMP_DIR} && tar -xzf opencli.tar.gz -C ${TEMP_DIR}opencli
-    rm -rf /usr/local/admin/scripts/
-    cp -r ${TEMP_DIR}opencli/opencli-main /usr/local/admin/scripts
+    #rm -rf /usr/local/admin/scripts/
+
+    find /usr/local/admin/scripts/ -mindepth 1 -name 'watcher' ! -name 'watcher.sh' -exec rm -rf {} +
+    
+    cp -r ${TEMP_DIR}opencli/ /usr/local/admin/scripts/
     rm ${TEMP_DIR}opencli.tar.gz 
     rm -rf ${TEMP_DIR}opencli
+
 
     cp /usr/local/admin/scripts/opencli /usr/local/bin/opencli
     chmod +x /usr/local/bin/opencli
