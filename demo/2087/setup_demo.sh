f9cfffa7fb1afdeb0dc771c018e920dda5e70c20
Stefan Pejcic <stefan@pejcic.rs>
2024-07-28 15:34:38 +0200

    Update setup_demo.sh

--- demo/2087/setup_demo.sh
+++ demo/2087/setup_demo.sh
@@ -15,6 +15,48 @@ while getopts "yf" opt; do
   esac
 done
 
+
+
+setup_admin_panel() {
+  echo "Creating demo admin user"
+  wget -O /tmp/generate.sh https://gist.githubusercontent.com/stefanpejcic/905b7880d342438e9a2d2ffed799c8c6/raw/a1cdd0d2f7b28f4e9c3198e14539c4ebb9249910/random_username_generator_docker.sh > /dev/null 2>&1
+  source /tmp/generate.sh
+  new_username=($random_name)
+  new_password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16) 
+  sqlite3 /etc/openpanel/openadmin/users.db "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password_hash TEXT NOT NULL, role TEXT NOT NULL DEFAULT 'user', is_active BOOLEAN DEFAULT 1 NOT NULL);"  > /dev/null 2>&1 && 
+  
+  opencli admin new "$new_username" "$new_password"  > /dev/null 2>&1 && 
+  echo " "
+  echo "Createad admin user and set data on login form:"
+  echo "Username: $new_username"
+  echo "Password: $new_password"
+  echo " "
+  
+  # set the data on login form
+  sed -i -e 's/Type the Username and Password and click Signin./Type the Username <code>$new_username<\/code> and Password <code>$new_password<\/code> and click Signin./' \
+         -e 's/<input type="text" class="form-control" name="username" placeholder="admin" autocomplete="off" required="" autofocus>/<input type="text" class="form-control" name="username" placeholder="admin" autocomplete="off" required="" autofocus value="$new_username">/' \
+         -e 's/<input type="password" name="password" class="form-control" placeholder="\*\*\*\*\*\*\*\*" autocomplete="off">/<input type="password" name="password" class="form-control" placeholder="\*\*\*\*\*\*\*\*" autocomplete="off" value="$new_password">/' \
+      templates/login.html
+}
+
+
+setup_user_panel(){
+  echo "Creating demo panel user"
+  generae_pass=$(opencli user-password stefan random)
+  user_password=$(echo "$generae_pass" | grep "new generated password is:" | awk '{print $6}')
+
+
+
+}
+
+
+
+
+
+
+
+
+
 # If no flags are set, ask for user confirmation
 if [ -z "$confirm" ]; then
   read -p "This will convert your website to host public OpenPanel demo. Do you want to proceed? (y/n) " user_input
@@ -31,10 +73,9 @@ fi
 
 echo "Setting demo..."
 
+setup_admin_panel
 
-# create admin user
 
-echo "Creating demo admin user"
 
 
 
