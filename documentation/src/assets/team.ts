2b19d79bb56d10ff7835e1ceb6b8eadc4d66b172
Stefan Pejcic <stefan@pejcic.rs>
2024-08-15 10:19:42 +0200

    Update team.ts

--- documentation/src/assets/team.ts
+++ documentation/src/assets/team.ts
@@ -3,22 +3,32 @@ export const team = [
         name: "Vladimir Duka",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/civan.png",
         role1: "Co-Founder",
-        role2: "CEO",
+        role2: "Project Manager",
     },
     {
         name: "Stefan Pejcic",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/omer.png",
         role1: "Co-Founder",
-        role2: "CTO",
+        role2: "CEO",
     },
     {
         name: "Radovan Jecmenica",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/eren.png",
-        role1: "Software Developer",
+        role1: "Frontend Developer",
     },
     {
         name: "Petar Curic",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
-        role1: "Product Designer",
+        role1: "Frontend Developer",
+    },
+    {
+        name: "Dragana Panic",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        role1: "Marketing",
+    },
+    {
+        name: "Nikola Markovic",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        role1: "QA Analyst",
     },
 ];
