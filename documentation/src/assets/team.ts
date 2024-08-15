08ced8696f53b668059f30170dadc81529496b6f
Stefan Pejcic <stefan@pejcic.rs>
2024-08-15 10:22:40 +0200

    Update team.ts

--- documentation/src/assets/team.ts
+++ documentation/src/assets/team.ts
@@ -1,34 +1,34 @@
 export const team = [
     {
         name: "Vladimir Duka",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/civan.png",
+        avatar: "https://media.licdn.com/dms/image/D4D03AQGE_nG9KEcEhA/profile-displayphoto-shrink_200_200/0/1663466730080?e=2147483647&v=beta&t=AChxbTkfMxbB0uwG9CUWeZE1SUiT6HdQdF2k2kwXM4E",
         role1: "Co-Founder",
         role2: "Project Manager",
     },
     {
         name: "Stefan Pejcic",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/omer.png",
+        avatar: "https://media.licdn.com/dms/image/C4D03AQEpX_VV7sh5kQ/profile-displayphoto-shrink_200_200/0/1634246783524?e=2147483647&v=beta&t=v4wmXJwRkoJ2UHzQuLJL6dl-Y8U-DKf0m0zUuL_BW9g",
         role1: "Co-Founder",
         role2: "CEO",
     },
     {
         name: "Radovan Jecmenica",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/eren.png",
+        avatar: "https://media.licdn.com/dms/image/C5603AQH320bWXbQZXA/profile-displayphoto-shrink_200_200/0/1634293404717?e=2147483647&v=beta&t=5p7E5xoyj_wuI8z_MlfgHAeLAc25kJlOLC2Y6BaGE9k",
         role1: "Frontend Developer",
     },
     {
         name: "Petar Curic",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        avatar: "https://media.licdn.com/dms/image/v2/D5603AQEy4Mls-Fqo3Q/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1629923454941?e=1729123200&v=beta&t=HFdsd6n_UNQ_t8acQlF_mty_OaETTn6I3A78x0VReC0",
         role1: "Frontend Developer",
     },
     {
         name: "Dragana Panic",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        avatar: "https://i.imgur.com/Rspdz2w.png",
         role1: "Marketing",
     },
     {
         name: "Nikola Markovic",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        avatar: "https://i.imgur.com/LrNZOaS.png",
         role1: "QA Analyst",
     },
 ];
