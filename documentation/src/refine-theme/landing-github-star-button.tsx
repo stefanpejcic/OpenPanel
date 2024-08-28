32cce3dd746eafc7b6785a68b6e7026c852f5c02
Stefan Pejcic <stefan@pejcic.rs>
2024-08-28 19:38:48 +0200

    Update landing-github-star-button.tsx

--- documentation/src/refine-theme/landing-github-star-button.tsx
+++ documentation/src/refine-theme/landing-github-star-button.tsx
@@ -3,25 +3,31 @@ import React from "react";
 export const LandingGithubStarButton = () => {
 
     return (
-        <a
-            href="/demo"
-            rel="noreferrer"
-            className={clsx(
-                "flex gap-2 items-center",
-                "font-normal",
-                "text-sm leading-6",
-                "text-gray-500 dark:text-gray-400",
-                "hover:text-gray-400 dark:hover:text-gray-300",
-                "hover:no-underline",
-                "transition-colors",
-                "duration-200",
-                "ease-in-out",
-            )}
-        >
-
-            <div className={clsx("flex items-center", "w-10 h-6")}>
-                    <span>Demo</span>
-            </div>
-        </a>
+                        <Link
+                            to="https://demo.openpanel.org/openpanel/"
+                            className={clsx(
+                                "self-start",
+                                "rounded-3xl",
+                                "!text-gray-0 dark:!text-gray-900",
+                                "bg-refine-blue dark:bg-refine-cyan-alt",
+                                "transition-[filter]",
+                                "duration-150",
+                                "ease-in-out",
+                                "hover:brightness-110",
+                                "py-2",
+                                "pl-7 pr-8",
+                                "landing-md:px-8",
+                                "landing-lg:pl-7 landing-lg:pr-8",
+                                "flex",
+                                "items-center",
+                                "justify-center",
+                                "gap-2",
+                                "hover:!no-underline",
+                            )}
+                        >
+                    <span className={clsx("text-base", "font-semibold")}>
+                        Try Demo
+                    </span>
+                </Link>
     );
 };
