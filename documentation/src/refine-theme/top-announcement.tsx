498b9f46e548c90bf41e6166a1d0d80816c13bab
Stefan Pejcic <stefan@pejcic.rs>
2024-09-06 06:55:58 +0200

    Update top-announcement.tsx

--- documentation/src/refine-theme/top-announcement.tsx
+++ documentation/src/refine-theme/top-announcement.tsx
@@ -110,7 +110,7 @@ export const TopAnnouncement = () => {
 const Text = () => {
     return (
         <a
-            href="/blog/openpanel-rhel-support"
+            href="/beta"
             rel="noreferrer"
             className={clsx(
                 "relative lg:absolute",
@@ -131,7 +131,10 @@ const Text = () => {
             )}
         >
             <div className={clsx("ml-2", "not-prose")}>
-                OpenPanel now supports all major RHEL distributions: AlmaLinux, Fedora, RockyLinux and CentOS
+                Unlock powerful features and effortlessly manage users and websites with  
+                {" "}
+                <span className={clsx("font-semibold")}>OpenPanel Enterprise</span>
+                {" "}
                 <span
                     className={clsx(
                         "text-refine-cyan-alt hover:text-refine-cyan-alt",
