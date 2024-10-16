a358c01421a5652938e65fe45e289a4c2f01d7d1
Stefan Pejcic <stefan@pejcic.rs>
2024-10-16 17:29:06 +0200

    Update UPDATE.sh

--- version/0.3.3/UPDATE.sh
+++ version/0.3.3/UPDATE.sh
@@ -252,10 +252,6 @@ download_new_admin() {
     chmod +x ${OPENADMIN_DIR}modules/security/csf.pl
     
     cd $OPENADMIN_DIR
-    # for 0.3.1 only!
-    pip3 install --force-reinstall zope.event || pip3 install --force-reinstall zope.event --break-system-packages
-    pip3 install --force-reinstall flask_caching || pip3 install --force-reinstall flask_caching --break-system-packages
-    pip3 install flask_caching # reported
     mv /etc/openpanel/openadmin/config/terms /etc/openpanel/openadmin/config/terms_accepted_on_update
 
 }
