bc72ab5cd5513d65f36fc368ad50fef76cb5349b
Stefan Pejcic <stefan@pejcic.rs>
2024-11-12 19:16:41 +0100

    Update INSTALL.sh

--- services/proxy/INSTALL.sh
+++ services/proxy/INSTALL.sh
@@ -8,7 +8,8 @@ mv Caddyfile /etc/caddy/Caddyfile
 service caddy restart
 
 # PHP
-
 chown caddy:caddy  /run/php-fpm/www.sock
+sed -i 's/^user = .*/user = caddy/' /etc/php-fpm.d/www.conf
+sed -i 's/^group = .*/group = caddy/' /etc/php-fpm.d/www.conf
 service php-fpm restart
 
