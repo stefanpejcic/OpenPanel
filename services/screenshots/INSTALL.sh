3212cab08a375a4c73cfbe624f885b30c5d43a25
Stefan Pejcic <stefan@pejcic.rs>
2024-11-13 13:47:43 +0100

    Create INSTALL.sh

---
+++ services/screenshots/INSTALL.sh
@@ -0,0 +1,35 @@
+
+
+apt install python3-pip
+
+apt  /home/screenshot/requirements.txt
+
+# DEV venv/bin/python -m flask run --host=0.0.0.0 -p 80
+
+
+
+cd /home/screenshot/
+python3 -m venv venv
+
+apt install python3.12-venv
+
+source venv/bin/
+
+apt install python3-playwright
+
+
+
+   62  sudo ufw reset
+   63  ufw default deny incoming
+   64  ufw default allow outgoing
+   65  ufw allow from 82.117.216.242
+   66  ufw allow from 185.119.89.240
+   67  ufw enable
+   96  ufw allow from 10.116.0.5
+   97  ufw allow from 24.144.64.6
+  113  ufw reload
+
+
+
+# @reboot /bin/bash /home/screenshot/start.sh
+
