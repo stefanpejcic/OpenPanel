66450230fe9bde62339b9e2c556cc657eba5f452
Stefan Pejcic <stefan@pejcic.rs>
2024-11-16 10:12:50 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1619,7 +1619,11 @@ install_openadmin(){
 
         git clone -b $py_enchoded_for_distro --single-branch https://github.com/stefanpejcic/openadmin $OPENPADMIN_DIR
         cd $OPENPADMIN_DIR
-        pip install --default-timeout=3600 -r requirements.txt  > /dev/null 2>&1 || pip install --default-timeout=3600 -r requirements.txt --break-system-packages  > /dev/null 2>&1
+
+        # for 0.3.5 ONLY!
+        sed -i '/^gevent$/s/.*/gevent==24.11.1/' requirements.txt > /dev/null 2>&1
+
+        pip install --default-timeout=3600 --force-reinstall --ignore-installed -r requirements.txt  > /dev/null 2>&1 || pip install --default-timeout=3600 --force-reinstall --ignore-installed -r requirements.txt --break-system-packages  > /dev/null 2>&1
 
      # on debian12 yaml is also needed to read conf files!
      if [ -f /etc/os-release ] && grep -q "Debian" /etc/os-release; then
