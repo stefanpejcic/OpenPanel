b5a2d54e4154440d0ddf47b8dab45a44c113a081
Stefan Pejcic <stefan@pejcic.rs>
2024-11-19 14:04:01 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,7 +11,7 @@
 # Usage:                   bash <(curl -sSL https://openpanel.org)
 # Author:                  Stefan Pejcic <stefan@pejcic.rs>
 # Created:                 11.07.2023
-# Last Modified:           18.11.2024
+# Last Modified:           19.11.2024
 #
 ################################################################################
 
