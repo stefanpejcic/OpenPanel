7166335bf2259a8a9e03edc4c9517db72bfa083b
Stefan Pejcic <stefan@pejcic.rs>
2024-12-03 09:01:38 +0100

    Update build-new-demo.sh

--- .github/workflows/build-new-demo.sh
+++ .github/workflows/build-new-demo.sh
@@ -25,6 +25,42 @@ jobs:
           echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
           chmod 600 ~/.ssh/id_rsa
 
+      - name: Run Rebuild Script and Capture Snapshot ID
+        id: rebuild
+        run: |
+          chmod +x demo/rebuild.sh
+          output=$(./demo/rebuild.sh)
+          echo "$output"
+          snapshot_id=$(echo "$output" | grep -oP 'Snapshot ID: \K.*')
+          echo "Snapshot ID captured: $snapshot_id"
+          echo "snapshot_id=$snapshot_id" >> $GITHUB_ENV
+
+      - name: Update SNAPSHOT_ID.env
+        run: |
+          echo "SNAPSHOT_ID=${{ env.snapshot_id }}" > demo/SNAPSHOT_ID.env
+
+      - name: Commit and Push Updates
+        run: |
+          git config --global user.name "github-actions[bot]"
+          git config --global user.email "github-actions[bot]@users.noreply.github.com"
+          git add demo/SNAPSHOT_ID.env
+          git commit -m "Demo rebuilt with new version - snapshot id: ${{ env.snapshot_id }}"
+          git push
+
+
+
+
+
+
+
+
+
+      - name: Set up SSH key
+        run: |
+          mkdir -p ~/.ssh
+          echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
+          chmod 600 ~/.ssh/id_rsa
+
       # Run the rebuild script
       - name: Rebuilding the demo site for new OpenPanel version
         run: |
