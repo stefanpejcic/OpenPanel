47f944d81e4309de12c5b50a2cb501fc6045066a
Stefan Pejcic <stefan@pejcic.rs>
2025-03-07 19:55:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -677,6 +677,12 @@ tweak_ssh(){
    fi
 
 
+
+	#echo "Generating MOTD for users.. *(auto-refreshed every 10hrs)"
+	opencli server-motd   > /dev/null 2>&1
+	ln -s /etc/openpanel/ssh/99-ssh-openpanel-users-settings.conf /etc/ssh/sshd_config.d/99-ssh-openpanel-users-settings.conf  > /dev/null 2>&1
+	chmod +x /etc/openpanel/ssh/lazydocker.sh > /dev/null 2>&1
+
 	# ssh on debian, sshd on rhel
 	if [ "$PACKAGE_MANAGER" == "dnf" ] || [ "$PACKAGE_MANAGER" == "yum" ]; then
 	 	systemctl restart sshd  > /dev/null 2>&1
