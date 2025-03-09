1cd3837aae29f213a289d21dacd9beed3bbc3a4d
Stefan Pejcic <stefan@pejcic.rs>
2025-03-09 16:20:12 +0100

    Create UPDATE.sh

---
+++ version/1.1.2/UPDATE.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# extra step for 1.1.1
+
+
+wget -O /etc/openpanel/docker/compose/1.0/docker-compose.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/1.0/docker-compose.yml
