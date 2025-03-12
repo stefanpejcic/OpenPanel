ad311af7db917aa99f5293120a2817efb88e82d8
Stefan Pejcic <stefan@pejcic.rs>
2025-03-13 00:09:04 +0100

    Update menu.tsx

--- website/src/refine-theme/common-header/menu.tsx
+++ website/src/refine-theme/common-header/menu.tsx
@@ -22,50 +22,6 @@ export const Menu: React.FC = () => {
                             key={`navbar-${item.label}`}
                             item={item}
                         >
-                            {item.label === "Products" && (
-                                <>
-                                    <div
-                                        className={clsx(
-                                            "grid grid-cols-2 gap-4",
-                                            "p-4",
-                                            "w-[672px]",
-                                            "dark:bg-gray-900 bg-white",
-                                        )}
-                                    >
-                                        {item.items.map((subItem) => (
-                                            <MenuItem
-                                                key={subItem.label}
-                                                item={subItem}
-                                            />
-                                        ))}
-                                    </div>
-                                    <Link
-                                        to="https://github.com/stefanpejcic/openpanel/"
-                                        className="no-underline"
-                                    >
-                                        <div
-                                            className={clsx(
-                                                "border-t dark:border-gray-700 border-gray-300",
-                                                "dark:bg-gray-800 bg-gray-100",
-                                                "flex items-center",
-                                                "py-4 px-7",
-                                            )}
-                                        >
-                                            <GithubStarIcon />
-                                            <div
-                                                className={clsx(
-                                                    "ml-4",
-                                                    "dark:text-gray-400 text-gray-600",
-                                                )}
-                                            >
-                                                If you like OpenPanel, don’t forget
-                                                to star us on GitHub!
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </>
-                            )}
-
                             {item.label === "Community" && (
                                 <>
                                     <div
