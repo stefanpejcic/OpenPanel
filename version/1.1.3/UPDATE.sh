341a18d26ff6242e2d9fe58ed02ee5178c26008e
Stefan Pejcic <stefan@pejcic.rs>
2025-03-14 00:43:11 +0100

    Update UPDATE.sh

--- version/1.1.3/UPDATE.sh
+++ version/1.1.3/UPDATE.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# replace path in template
-sed -i 's|/opt/bitnami/php/etc/conf.d|/usr/local/etc/php/|g' /etc/openpanel/docker/compose/1.0/docker-compose.yml
+#  force templates!
+wget -O /etc/openpanel/docker/compose/1.0/docker-compose.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/1.0/docker-compose.yml
+wget -O /etc/openpanel/docker/compose/1.0/.env https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/1.0/.env
 
-# replace for existing users 
+# backup first!
+find /home/* -maxdepth 1 -name docker-compose.yml -exec cp {} {}.1.1.2_backup_before_update \;
+find /home/* -maxdepth 1 -name .env -exec cp {} {}.1.1.2_backup_before_update \;
+
+# then replace for existing users 
 sed -i 's|/opt/bitnami/php/etc/conf.d|/usr/local/etc/php/|g' /home/*/docker-compose.yml
