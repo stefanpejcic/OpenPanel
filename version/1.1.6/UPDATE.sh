b28194d5690ead5465c4108f94edf74194dbbe6e
Stefan Pejcic <stefan@pejcic.rs>
2025-03-24 13:09:06 +0100

    Create UPDATE.sh

---
+++ version/1.1.6/UPDATE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+```
+LoadModule proxy_module modules/mod_proxy.so
+LoadModule proxy_http_module modules/mod_proxy_http.so
+LoadModule proxy_fcgi_module modules/mod_proxy_fcgi.so
+```
+
+u /home/*/httpd.conf
+
+
+nadje 
+
+```
+Listen 80
+```
+
+cd /gome/USER 
+
+docker --context USER compose down apache
+docker --context USER compose up -d apache
