de916990c4e86a3e0b30cd0f293945aa73af14a7
Stefan Pejcic <stefan@pejcic.rs>
2025-03-24 13:10:45 +0100

    Update UPDATE.sh

--- version/1.1.6/UPDATE.sh
+++ version/1.1.6/UPDATE.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+wget -O /etc/openpanel/apache/httpd.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/apache/httpd.conf
+
+
+
+
+DOLE:
+
 
 ```
 LoadModule proxy_module modules/mod_proxy.so
