74c1c842c7482933fd1d10f28a3347515ea46c79
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2025-04-11 15:44:39 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,7 +39,7 @@ const siteConfig = {
                               process.env.DISABLE_VERSIONING === "true",
                           versions: {
                               current: {
-                                  label: "1.1.8",
+                                  label: "1.2.1",
                               },
                           },
                           lastVersion: "current",
