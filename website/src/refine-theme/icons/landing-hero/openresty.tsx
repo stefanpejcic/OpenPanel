06953b58b931cd2681205f39446edf5b03bf9c1f
Stefan Pejcic <stefan@pejcic.rs>
2025-04-15 11:00:58 +0200

    Update and rename appwrite.tsx to openresty.tsx

--- website/src/refine-theme/icons/landing-hero/openresty.tsx
+++ website/src/refine-theme/icons/landing-hero/openresty.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const LandingHeroAppwriteIcon = (props: React.SVGProps<SVGSVGElement>) => (
+export const LandingHeroOpenRestyIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
         width={24}
         height={64}
