a8bcb49d7360b3a96047baf353096b0d18357492
Stefan Pejcic <stefan@pejcic.rs>
2025-05-03 10:21:38 +0200

    Update UPDATE.sh

--- version/1.2.7/UPDATE.sh
+++ version/1.2.7/UPDATE.sh
@@ -15,3 +15,9 @@ wget -O /etc/openpanel/nginx/vhosts/1.1/docker_nginx_domain.conf https://raw.git
 wget -O /etc/openpanel/nginx/vhosts/1.1/docker_openresty_domain.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/nginx/vhosts/1.1/docker_openresty_domain.conf
 wget -O /etc/openpanel/nginx/vhosts/1.1/docker_apache_domain.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/nginx/vhosts/1.1/docker_apache_domain.conf
 
+
+echo ""
+echo "📥 Updating docker-compose.yml file for new domains.."
+
+cp /etc/openpanel/docker/compose/1.0/docker-compose.yml /etc/openpanel/docker/compose/1.0/127-docker-compose.yml
+wget -O /etc/openpanel/docker/compose/1.0/docker-compose.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/1.0/docker-compose.yml
