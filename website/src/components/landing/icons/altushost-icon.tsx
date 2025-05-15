5fd7cc97cf3f52608b1591e749fc015a1723fde1
Stefan Pejcic <stefan@pejcic.rs>
2025-05-15 16:31:33 +0200

    Update and rename cisco-icon.tsx to altushost-icon.tsx

--- website/src/components/landing/icons/altushost-icon.tsx
+++ website/src/components/landing/icons/altushost-icon.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { SVGProps } from "react";
 
-export const CiscoIcon = (props: SVGProps<SVGSVGElement>) => (
+export const AltusHostIcon = (props: SVGProps<SVGSVGElement>) => (
 
    <svg
         xmlns="http://www.w3.org/2000/svg"
