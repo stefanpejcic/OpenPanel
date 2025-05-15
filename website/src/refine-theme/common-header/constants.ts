11311a6bdaf66954551360a11d3779ec6d978b24
Stefan Pejcic <stefan@pejcic.rs>
2025-05-15 17:55:15 +0200

    Update constants.ts

--- website/src/refine-theme/common-header/constants.ts
+++ website/src/refine-theme/common-header/constants.ts
@@ -34,44 +34,83 @@ export type NavbarItemType = {
 
 export type MenuItemType = NavbarPopoverItemType | NavbarItemType;
 
+
 export const MENU_ITEMS: MenuItemType[] = [
-    {
-       isPopover: false,
-       label: "Install",
-       href: "/install",
-    },
-    {
-       isPopover: false,
-       label: "Docs",
-       href: "/docs/",
-    },   
-    {
-       isPopover: false,
-       label: "Forums",
-       href: "https://community.openpanel.org/",
-    },       
-  //  {
-   //     isPopover: true,
-  //      label: "Company",
-  //      items: [
-   //         {
-  //              label: "About Us",
-  //              description: "Team & company information.",
-       //         link: "/about",
-    //            icon: AboutUsIcon,
-  //          },
-     //       {
-    //            label: "Become a Partner",
-      //          description: "Help us spread the word!",
-      //          link: "mailto:info@openpanel.co",
-      //          icon: StoreIcon,
-    //        },
-     //       {
-     //           label: "Meet OpenPanel",
-     //           description: "Call us for any questions",
-     //           link: "mailto:info@openpanel.co",
-     //           icon: MeetIcon,
-     //       },
-    //    ],
- //   },
+  {
+    isPopover: true,
+    label: "Resources",
+    items: [
+      {
+        label: "Documentation",
+        description: "Everything you need to get started.",
+        link: "/docs/",
+        icon: DocumentsIcon,
+      },
+      {
+        label: "Live Demo",
+        description: "Give OpenPanel or OpenAdmin a try.",
+        link: "/demo",
+        icon: TutorialIcon,
+      },
+      {
+        label: "Install Command",
+        description: "Generate command to customize installation.",
+        link: "/install",
+        icon: AwesomeIcon,
+      },
+      {
+        label: "Integrations",
+        description: "WHMCS, FOSSBilling, Blesta.",
+        link: "/features",
+        icon: IntegrationsIcon,
+      },
+      {
+        label: "How-to Guides",
+        description: "Most common tasks on OpenPanel.",
+        link: "/docs/articles/intro/",
+        icon: ExamplesIcon,
+      },
+      {
+        label: "Changelog",
+        description: "Latest version and changes.",
+        link: "/docs/changelog/intro/",
+        icon: BlogIcon,
+      },
+    ],
+  },
+  {
+    isPopover: false,
+    label: "Live Demo",
+    href: "/demo",
+    icon: NewBadgeIcon,
+  },
+  {
+    isPopover: false,
+    label: "Enterprise",
+    href: "/beta",
+  },
+  {
+    isPopover: true,
+    label: "Support",
+    items: [
+      {
+        label: "Forums",
+        description: "Join our Community forums!",
+        link: "https://community.openpanel.org/",
+        icon: AboutUsIcon,
+      },
+      {
+        label: "Discord",
+        description: "Join us on Discord.",
+        link: "https://discord.openpanel.com",
+        icon: StoreIcon,
+      },
+      {
+        label: "Contacy Us",
+        description: "Email us for any questions.",
+        link: "mailto:info@openpanel.com",
+        icon: MeetIcon,
+      },
+    ],
+  },
 ];
