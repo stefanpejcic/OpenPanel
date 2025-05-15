8d2ee42b633c088d3d18e9041ae4e7481d75ebf2
Stefan Pejcic <stefan@pejcic.rs>
2025-05-15 16:32:14 +0200

    Update and rename salesforce-icon.tsx to hostkey-icon.tsx

--- website/src/components/landing/icons/hostkey-icon.tsx
+++ website/src/components/landing/icons/hostkey-icon.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { SVGProps } from "react";
 
-export const SalesforceIcon = (props: SVGProps<SVGSVGElement>) => (
+export const HostkeyIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={187}
