82ad7b0985aadefb6a843e497d6b18bd81918566
Stefan Pejcic <stefan@pejcic.rs>
2025-05-15 17:57:10 +0200

    Update landing-github-star-button.tsx

--- website/src/refine-theme/landing-github-star-button.tsx
+++ website/src/refine-theme/landing-github-star-button.tsx
@@ -4,14 +4,29 @@ export const LandingGithubStarButton = () => {
 
     return (
         <a
-            href="https://my.openpanel.com/index.php?rp=/store/openpanel/enterprise-license"
-            target="_blank"
+            href="https://my.openpanel.com/clientarea.php"
             className={clsx(
+                                "self-start",
+                                "rounded-3xl",
+                                "!text-gray-0 dark:!text-gray-900",
+                                "bg-refine-blue dark:bg-refine-cyan-alt",
+                                "transition-[filter]",
+                                "duration-150",
+                                "ease-in-out",
+                                "hover:brightness-110",
+                                "py-2",
+                                "pl-7 pr-8",
+                                "landing-md:px-8",
+                                "landing-lg:pl-7 landing-lg:pr-8",
+                                "flex",
+                                "items-center",
+                                "justify-center",
+                                "gap-2",
                                 "hover:!no-underline",
                             )}
                         >
                     <span className={clsx("text-base", "font-semibold")}>
-                        Enterprise
+                        Account Login
                     </span>
                 </a>
     );
