d9189bce051ca63ef36322c7958b539fb374378f
Stefan Pejcic <stefan@pejcic.rs>
2025-05-16 12:49:00 +0200

    Update UPDATE.sh

--- version/1.3.0/UPDATE.sh
+++ version/1.3.0/UPDATE.sh
@@ -2,6 +2,10 @@
 
 echo ""
 echo "📥 Updating openadmin service.."
-
 wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
 systemctl restart admin  > /dev/null 2>&1
+
+
+echo ""
+echo "📥 Updating rootless.json.."
+wget -O /etc/openpanel/docker/daemon/rootless.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/daemon/rootless.json
