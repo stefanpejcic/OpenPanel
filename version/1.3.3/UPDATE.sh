7cdb79246496b17296a867b3dd1898ccb6563872
Stefan Pejcic <stefan@pejcic.rs>
2025-05-27 12:02:35 +0200

    Create UPDATE.sh

---
+++ version/1.3.3/UPDATE.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+echo ""
+echo "📥 Editing SSH welcome message for administrators.."
+wget -O /etc/openpanel/ssh/admin_welcome.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/ssh/admin_welcome.sh
+chmod +x /etc/openpanel/ssh/admin_welcome.sh
+
+
+
+mkdir -p /etc/openpanel/openpanel/features
+
+echo ""
+echo "📥 Downloading default feature set for new users.."
+wget -O /etc/openpanel/openpanel/features/default.txt https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/features/default.txt
+
+echo ""
+echo "Setting default feature set for all existing users.."
+
+for dir in /home/*; do
+  if [ -d "$dir" ]; then
+    if [ -f "$dir/.env" ]; then
+      ln -sf /etc/openpanel/openpanel/features/default.txt "$dir/features.txt"
+      echo "- Default feature set applied for user $dir"
+    fi
+  fi
+done
