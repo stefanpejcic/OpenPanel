f44146241625c867eaafe7795d4b83f86ec5de94
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2025-05-29 14:57:57 +0200

    Update UPDATE.sh

--- version/1.3.4/UPDATE.sh
+++ version/1.3.4/UPDATE.sh
@@ -3,3 +3,12 @@
 wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
 
 wget -O /etc/openpanel/openpanel/features/default.txt https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/features/default.txt
+
+CADDYFILE="/etc/openpanel/caddy/Caddyfile"
+
+if grep -q "^ *admin off" "$CADDYFILE"; then
+    sed -i 's/^ *admin off/#admin off/' "$CADDYFILE"
+    echo "'admin off' has been commented out."
+else
+    echo "No 'admin off' line found or it's already commented."
+fi
