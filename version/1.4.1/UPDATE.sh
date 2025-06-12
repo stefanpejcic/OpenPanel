971a570a1c297614402f09c797a3f329d0b23afe
Stefan Pejcic <stefan@pejcic.rs>
2025-06-12 17:36:55 +0200

    Create UPDATE.sh

---
+++ version/1.4.1/UPDATE.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget --inet4-only -O /etc/openpanel/openpanel/conf/knowledge_base_articles.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/conf/knowledge_base_articles.json
+
+
+
