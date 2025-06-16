3f561e3a2f69b364278af8ba51a047fc51f1968b
Stefan Pejcic <stefan@pejcic.rs>
2025-06-16 12:10:44 +0200

    Create UPDATE.sh

---
+++ version/1.4.2/UPDATE.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
