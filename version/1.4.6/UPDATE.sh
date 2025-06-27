6cee70a9ca7e9b887e797488a82e0adbdaac0d96
Stefan Pejcic <stefan@pejcic.rs>
2025-06-27 21:12:59 +0200

    Create UPDATE.sh

---
+++ version/1.4.6/UPDATE.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
