edc22ef3d55505bdedc4f6a2daed52b240b48ac2
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-11 12:36:31 +0200

    Update constants.ts

--- website/src/assets/week-of-refine/constants.ts
+++ website/src/assets/week-of-refine/constants.ts
@@ -3,22 +3,22 @@ import { StrapiWithText, SupabaseWithText } from "../integration-icons";
 
 export const weekOfRefineCards: CardProps[] = [
     {
-        title: "Docker for Beginners",
+        title: "FOSSBilling",
         imgURL: "https://refine.ams3.cdn.digitaloceanspaces.com/week-of-refine/week-of-refine-invoicer.png",
         dateRange: "February 7, 2024",
         description:
-            "Introduction to Docker and why we decided to use it for OpenPanel.",
+            "FOSSBilling module for OpenPanel.",
         logo: StrapiWithText,
         bgLinearGradient:
             "bg-week-of-refine-strapi-card-light dark:bg-week-of-refine-strapi-card",
         link: "/week-of-refine-strapi",
     },
     {
-        title: "Troubleshooting DNS",
+        title: "WHMCS",
         imgURL: "https://refine.ams3.cdn.digitaloceanspaces.com/week-of-refine/week-of-refine-pixels.png",
         dateRange: "December 14, 2023",
         description:
-            "Troubleshooting DNS zone problems and solutions.",
+            "OpenPanel Account provisioning module for WHMCS.",
         logo: SupabaseWithText,
         bgLinearGradient:
             "bg-week-of-refine-supabase-card-light dark:bg-week-of-refine-supabase-card",
