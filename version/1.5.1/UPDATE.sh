276f5f81d1304172f8ce1fbc8d52db71c1c84a03
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-24 08:17:54 +0200

    Update UPDATE.sh

--- version/1.5.1/UPDATE.sh
+++ version/1.5.1/UPDATE.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
 wget -O /etc/openpanel/ftp/Dockerfile https://raw.githubusercontent.com/stefanpejcic/OpenPanel-FTP/refs/heads/master/Dockerfile
+
+
+wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
