73cbb07473918f505334ebac05cc8c177117d524
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-30 16:01:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,6 +54,10 @@ CONFIG_FILE="${ETC_DIR}openpanel/conf/openpanel.config"               # main con
 
 exec > >(tee -a "$LOG_FILE") 2>&1
 
+
+
+echo "# restart_flag" > "/root/openpanel_restart_needed"
+
 # ======================================================================
 # Helper functions that are not mandatory and should not be modified
 
