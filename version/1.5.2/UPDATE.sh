aae262d40ce3f1726e5a37d3d40a2bc93228f07a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-30 17:09:48 +0200

    Update UPDATE.sh

--- version/1.5.2/UPDATE.sh
+++ version/1.5.2/UPDATE.sh
@@ -2,6 +2,8 @@
 
 #wget -O /etc/openpanel/ftp/start_vsftpd.sh https://raw.githubusercontent.com/stefanpejcic/OpenPanel-FTP/refs/heads/master/start_vsftpd.sh
 
+wget -O /etc/openpanel/ftp/vsftpd.conf https://raw.githubusercontent.com/stefanpejcic/OpenPanel-FTP/refs/heads/master/vsftpd.conf
+
 
 touch /root/openpanel_restart_needed
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
