a9b3bcb9347587543e8b6c03e47cdcd492d02bf7
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-04 19:02:49 +0200

    Update UPDATE.sh

--- version/1.5.4/UPDATE.sh
+++ version/1.5.4/UPDATE.sh
@@ -2,3 +2,5 @@
 
 # https://github.com/shinsenter/php/discussions/301
 sed -i '/command: >/{N;s/command: >\n *sh -c "php-fpm --allow-to-run-as-root"/command: php-fpm --allow-to-run-as-root/}' /home/*/docker-compose.yml
+
+sed -i '/command: >/{N;s/command: >\n *sh -c "php-fpm --allow-to-run-as-root"/command: php-fpm --allow-to-run-as-root/}' /etc/openpanel/docker/compose/1.0/docker-compose.yml
