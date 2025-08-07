7a7f76ea84b8ce261e0222c421cafefb7603857c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-07 14:07:03 +0200

    Update UPDATE.sh

--- version/1.5.4/UPDATE.sh
+++ version/1.5.4/UPDATE.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # https://github.com/shinsenter/php/discussions/301
-sed -i '/command: >/{N;s/command: >\n *sh -c "php-fpm --allow-to-run-as-root"/command: php-fpm --allow-to-run-as-root/}' /home/*/docker-compose.yml
+echo ""
+echo "Applying fix for https://github.com/shinsenter/php/discussions/301"
 
+for file in /home/*/docker-compose.yml; do
+  cp "$file" "$file.154_UPDATE.bak"
+  echo "Editing $file"
+
+  sed -i '/command: >/{N;s/command: >\n *sh -c "php-fpm --allow-to-run-as-root"/command: php-fpm --allow-to-run-as-root/}' "$file"
+done
+echo ""
+
+echo "Editing tempalte used for new users.."
 sed -i '/command: >/{N;s/command: >\n *sh -c "php-fpm --allow-to-run-as-root"/command: php-fpm --allow-to-run-as-root/}' /etc/openpanel/docker/compose/1.0/docker-compose.yml
 
+echo ""
 echo "🔍 Checking if Apache is installed..."
 
 # Detect package manager
