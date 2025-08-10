ae669506e802855c264ab2d42698f0053de0afb6
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-10 13:43:22 +0200

    Update UPDATE.sh

--- version/1.5.6/UPDATE.sh
+++ version/1.5.6/UPDATE.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# datatracker.ietf.org/doc/html/rfc1035#autoid-31
+# https://my.openpanel.com/adminad/supporttickets.php?action=view&id=3937
+mysql --database=panel -e "
+ALTER TABLE domains CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE sites CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+"
 
+
+# datatracker.ietf.org/doc/html/rfc1035#autoid-31
 for file in /etc/openpanel/bind9/zone_template.txt /etc/openpanel/bind9/zone_template_ipv6.txt; do
     sed -i 's/\(@[[:space:]]\+IN[[:space:]]\+SOA[[:space:]]\+{ns1}\.\)[[:space:]]\+{ns2}\./\1 {rpemail}./' "$file"
 done
