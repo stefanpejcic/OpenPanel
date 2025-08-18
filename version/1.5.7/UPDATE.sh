a31487a024301afa94370b2ec24bd698933264b6
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-18 13:06:11 +0200

    Update UPDATE.sh

--- version/1.5.7/UPDATE.sh
+++ version/1.5.7/UPDATE.sh
@@ -2,3 +2,11 @@
 
 echo "Adding Mautic module.."
 wget -O /etc/openpanel/openadmin/config/features.json  https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
+
+
+
+echo "Fix for crons page..
+"
+FILE="/etc/cron.d/openpanel"
+sed -i 's|root /bin/bash |root |' "$FILE"
+sed -i 's|/usr/local/opencli|/usr/local/bin/opencli|g' "$FILE"
