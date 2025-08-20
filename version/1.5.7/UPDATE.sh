a38f3122395f6477ab9d795a04ba3fbc8a9e5aa8
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-20 19:49:44 +0200

    Update UPDATE.sh

--- version/1.5.7/UPDATE.sh
+++ version/1.5.7/UPDATE.sh
@@ -3,17 +3,18 @@
 echo "Adding Mautic, SSL and Docroot modules.."
 wget -O /etc/openpanel/openadmin/config/features.json  https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
 
+echo "Fix for '/root/openpanel_restart_needed'"
+wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
 
 
-echo "Fix for crons page..
-"
+
+echo "Fix for crons page bug in OpenAdmin.."
 FILE="/etc/cron.d/openpanel"
 sed -i 's|root /bin/bash |root |' "$FILE"
 sed -i 's|/usr/local/opencli|/usr/local/bin/opencli|g' "$FILE"
 
 
 echo "OpenLitespeed.."
-
 mkdir -p /etc/openpanel/openlitespeed
 wget -O /etc/openpanel/nginx/vhosts/1.1/docker_openlitespeed_domain.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/nginx/vhosts/1.1/docker_openlitespeed_domain.conf
 wget -O /etc/openpanel/openlitespeed/httpd_config.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openlitespeed/httpd_config.conf
