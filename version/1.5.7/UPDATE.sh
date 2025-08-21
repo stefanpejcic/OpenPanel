5f203351864bb06e09da5c3dc38f9f2cd100eafe
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-21 15:05:49 +0200

    Update UPDATE.sh

--- version/1.5.7/UPDATE.sh
+++ version/1.5.7/UPDATE.sh
@@ -20,3 +20,20 @@ wget -O /etc/openpanel/nginx/vhosts/1.1/docker_openlitespeed_domain.conf https:/
 wget -O /etc/openpanel/openlitespeed/httpd_config.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openlitespeed/httpd_config.conf
 wget -O /etc/openpanel/openlitespeed/start.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openlitespeed/start.sh
 chmod +x /etc/openpanel/openlitespeed/start.sh
+
+# update .env
+
+ENV_FILE="/etc/openpanel/docker/compose/1.0/.env"
+DEFAULTS='
+# OPENLITESPEED
+OPENLLITESPEED_VERSION="latest"
+OPENLLITESPEED_CPU="1.0"
+OPENLLITESPEED_RAM="1.0G"
+'
+
+if ! grep -q "OPENLITESPEED" "$ENV_FILE"; then
+    echo "$DEFAULTS" >> "$ENV_FILE"
+fi
+
+
+# TODO: update "/etc/openpanel/docker/compose/1.0/docker-compose.yml"
