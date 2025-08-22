2e5c1ecc863efb2856ce873d1bc86a963050bb76
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-22 14:26:01 +0200

    Update UPDATE.sh

--- version/1.5.7/UPDATE.sh
+++ version/1.5.7/UPDATE.sh
@@ -6,7 +6,9 @@ wget -O /etc/openpanel/openadmin/config/features.json  https://raw.githubusercon
 echo "Fix for '/root/openpanel_restart_needed'"
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
 
+wget -O /etc/openpanel/openpanel/conf/blacklist_useragents.txt  https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/conf/blacklist_useragents.txt
 
+docker restart openpanel
 
 echo "Fix for crons page bug in OpenAdmin.."
 FILE="/etc/cron.d/openpanel"
