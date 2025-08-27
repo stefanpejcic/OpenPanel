14517ebb09d29e0466cbc78274b9aa6c9ad5d04a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-27 15:10:11 +0200

    Update index.tsx

--- website/src/pages/support/index.tsx
+++ website/src/pages/support/index.tsx
@@ -18,7 +18,7 @@ const PrivacyPolicy: React.FC = () => {
                     <h1>Support</h1>
                     <p><strong>Don't worry - we're here to help</strong></p>
                     <p>Make the most of your OpenPanel deployments, find quick solutions to common problems, and get direct assistance from the developers of OpenPanel and OpenAdmin. Usually, the quickest way to get answers is as follows:</p>
-                
+
                     <h3>Step 1: Search for similar questions</h3>
                     <p>If your question has been asked before, you might be able to find an answer in minutes and get your problem knocked out before we even know it exists.</p>
                 
@@ -26,21 +26,19 @@ const PrivacyPolicy: React.FC = () => {
                         <li><a href="/search">Search Documentation</a></li>
                         <li><a href="https://community.openpanel.org">Search Forums</a></li>
                     </ul>
-                
-                    <p>If a search does not provide good answers, proceed to step 2.</p>
-                                
+
                     <h3>Step 2a: Open a support ticket (Enterprise Edition)</h3>
                     <p>If you need fast and reliable support, open a premium ticket by sending us a new ticket.</p>
                     <ul>
                         <li><a href="https://my.openpanel.com/submitticket.php?step=2&deptid=1" target="_blank">Submit a ticket</a></li>
                     </ul>
-                
+
                     <h3>Step 2b: Post on forums (Community Edition)</h3>
                     <p>If you can wait for answers from our community and don't want to spend any money, ask your question in the forums. Every member of the OpenPanel staff also monitors the forums and tries to help out whenever we can. We generally prioritize paid support queries (we've got bills to pay, after all), but our forums are very active, and our community is knowledgeable.</p>
                     <ul>
                         <li><a href="https://community.openpanel.org">Ask the Community</a></li>
                     </ul>
-                                
+
                     <h3>Step 3: Found a bug?</h3>
                     <p>If you've found a bug in OpenPanel or OpenAdmin, Community or Enterprise edition, <a
                             href="https://github.com/stefanpejcic/openpanel/issues/new?assignees=&labels=Bug&projects=&template=1_Bug_report.yaml">please
