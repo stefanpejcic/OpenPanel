01786d6b8f6efec70475931684f265eb662c1dba
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-28 18:49:27 +0200

    Update community-hero-section.tsx

--- website/src/refine-theme/community-hero-section.tsx
+++ website/src/refine-theme/community-hero-section.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { EnterpriseGetInTouchButton } from "./enterprise-get-in-touch-button";
+import { EnterpriseGetInTouchButton } from "./community-download-button";
 import { CommonThemedImage } from "./common-themed-image";
 
 export const EnterpriseHeroSection = ({
