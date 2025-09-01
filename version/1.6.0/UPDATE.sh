12aa3fc585f708ec82a1a785a37f507c19f581b9
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-01 22:12:01 +0200

    Update UPDATE.sh

--- version/1.6.0/UPDATE.sh
+++ version/1.6.0/UPDATE.sh
@@ -3,9 +3,9 @@
 # https://github.com/stefanpejcic/OpenPanel/issues/671#issuecomment-3242952264
 
 echo "Adding support for custom SSL on OpenPanel GUI.."
-wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/fc1b6503cc62e3d61abab6473e42175e63fb7436/openpanel/service/service.config.py
+wget -4 -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/fc1b6503cc62e3d61abab6473e42175e63fb7436/openpanel/service/service.config.py
 docker restart openpanel
 
 echo "Adding support for custom SSL on OpenAdmin GUI.."
-wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+wget -4 -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
 service admin restart
