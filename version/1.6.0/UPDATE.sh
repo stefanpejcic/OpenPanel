d2062f5ea298361a9dd9f469ffb997dc7ef2b849
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-03 15:14:31 +0200

    Update UPDATE.sh

--- version/1.6.0/UPDATE.sh
+++ version/1.6.0/UPDATE.sh
@@ -3,17 +3,10 @@
 # https://github.com/stefanpejcic/OpenPanel/issues/671#issuecomment-3242952264
 
 echo "Adding support for custom SSL on OpenPanel GUI.."
-wget -4 -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/fc1b6503cc62e3d61abab6473e42175e63fb7436/openpanel/service/service.config.py
-docker restart openpanel
-
+wget -4 -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/fc1b6503cc62e3d61abab6473e42175e63fb7436/openpanel/service/service.config.py && docker restart openpanel
 
 echo "Adding support for custom SSL on OpenAdmin GUI.."
 wget -4 -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
-service admin restart
-
-
-echo "Adding support for custom SSL on OpenAdmin and OpenPanel.."
-wget -O  /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
 
 
 echo "Fixing 403 error on phpmyadmin autologin for users created before 1.4.5"
