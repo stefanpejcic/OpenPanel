1f20baa86d6b1cfa36cdf96a467ccd9afd8cb8e3
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-23 14:33:42 +0200

    Update landing-trusted-by-developers.tsx

--- website/src/refine-theme/landing-trusted-by-developers.tsx
+++ website/src/refine-theme/landing-trusted-by-developers.tsx
@@ -4,7 +4,7 @@ import {
     HostkeyIcon,
     AltusHostIcon,
     DigitalOceanIcon,
-    UnlimitedIcon,
+    LogicwebIcon,
 } from "../components/landing/icons";
 
 type Props = {
@@ -84,6 +84,6 @@ type IList = {
 const list: IList = [
     { icon: <HostkeyIcon />, id: 1, href: "https://hostkey.com/apps/hosting-control-panels/openpanel/" },
     { icon: <AltusHostIcon />, id: 2, href: "https://altushost.com" },
-    { icon: <DigitalOceanIcon />, id: 3, href: "https://digitalocean.com" },
-    { icon: <UnlimitedIcon />, id: 4, href: "https://unlimited.rs" },
+    { icon: <DigitalOceanIcon />, id: 3, href: "https://digitalocean.com/" },
+    { icon: <LogicwebIcon />, id: 4, href: "https://www.logicweb.com/" },
 ];
