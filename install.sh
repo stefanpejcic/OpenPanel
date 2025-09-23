d6f3c975b3be467a04ff484c4b03d9c31ccb3795
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-23 17:40:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 # Usage:                   bash <(curl -sSL https://openpanel.org)
 # Author:                  Stefan Pejcic <stefan@pejcic.rs>
 # Created:                 11.07.2023
-# Last Modified:           20.09.2025
+# Last Modified:           23.09.2025
 #
 ################################################################################
 
@@ -160,7 +160,7 @@ set_version_to_install() {
             PANEL_VERSION=$(echo "$response" | grep -oP '"latest_version":"\K[^"]+')
         fi
 
-        [[ "$PANEL_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || PANEL_VERSION="1.6.0"
+        [[ "$PANEL_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || PANEL_VERSION="1.6.1"
     fi
 }
 
