c695e1bd46e1a00c32950f5a1b33e19a0b97ae3f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-23 14:36:19 +0200

    Update hostkey-icon.tsx

--- website/src/components/landing/icons/hostkey-icon.tsx
+++ website/src/components/landing/icons/hostkey-icon.tsx
@@ -4,7 +4,7 @@ import { SVGProps } from "react";
 export const HostkeyIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={187}
+    width={150}
     height={80}
     viewBox="0 0 800 201.333"
     fill="none"
