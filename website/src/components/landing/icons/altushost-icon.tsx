0064abb2f9620fb8d6caacefbb2625084f202000
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-23 14:36:08 +0200

    Update altushost-icon.tsx

--- website/src/components/landing/icons/altushost-icon.tsx
+++ website/src/components/landing/icons/altushost-icon.tsx
@@ -5,7 +5,7 @@ export const AltusHostIcon = (props: SVGProps<SVGSVGElement>) => (
 
    <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={187}
+        width={150}
         height={80}
         viewBox="0 0 139 56"
         fill="none"
