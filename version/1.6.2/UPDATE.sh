c4f906d69428708c3ee9d76ec95572fd451d46ec
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-29 15:13:11 +0200

    Update UPDATE.sh

--- version/1.6.2/UPDATE.sh
+++ version/1.6.2/UPDATE.sh
@@ -5,6 +5,8 @@ echo "Upgrading CSF to Sentinel.."
 wget -O /etc/csf/csf.pl https://gist.githubusercontent.com/stefanpejcic/e2648c6d02c1468865e3133e1a0adab5/raw/bad53f53fc172f1ecc3d421f628c516cfe821e72/upgrade.csf.pl && csf -uf
 wget -O /etc/csf/csf.pl https://raw.githubusercontent.com/sentinelfirewall/sentinel/refs/heads/main/csf/csf.pl
 
+
+echo "Adding script for checking resellers usage.."
 CRON_FILE="/etc/cron.d/openpanel"
 CRON_JOB='0 */4 * * * root /usr/local/bin/opencli files-calculate_resellers_storage && echo "$(date) Calculated storage usage for all reseller accounts" >> /var/log/openpanel/admin/cron.log'
 
