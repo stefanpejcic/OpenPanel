5a988cb27039d5dc1aefc48e93af41444cf0ea5c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-18 15:14:11 +0200

    proba

--- website/src/refine-theme/common-header/constants.ts
+++ website/src/refine-theme/common-header/constants.ts
@@ -1,101 +1,115 @@
 import {
-    DocumentsIcon,
-    IntegrationsIcon,
-    TutorialIcon,
-    ExamplesIcon,
-    AwesomeIcon,
-    ContributeIcon,
-    RefineWeekIcon,
-    HackathonsIcon,
-    AboutUsIcon,
-    StoreIcon,
-    MeetIcon,
-    BlogIcon,
-    NewBadgeIcon,
+  DocumentsIcon,
+  IntegrationsIcon,
+  TutorialIcon,
+  ExamplesIcon,
+  AwesomeIcon,
+  ContributeIcon,
+  RefineWeekIcon,
+  HackathonsIcon,
+  AboutUsIcon,
+  StoreIcon,
+  MeetIcon,
+  BlogIcon,
+  NewBadgeIcon,
 } from "../icons/popover";
 
 export type NavbarPopoverItemType = {
-    isPopover: true;
+  isPopover: true;
+  label: string;
+  items: {
     label: string;
-    items: {
-        label: string;
-        description: string;
-        link: string;
-        icon: React.FC;
-    }[];
+    description: string;
+    link: string;
+    icon: React.FC;
+  }[];
 };
 
 export type NavbarItemType = {
-    isPopover?: false;
-    label: string;
-    icon?: React.FC;
-    href?: string;
+  isPopover?: false;
+  label: string;
+  icon?: React.FC;
+  href?: string;
 };
 
 export type MenuItemType = NavbarPopoverItemType | NavbarItemType;
 
 export const MENU_ITEMS: MenuItemType[] = [
-    {
-       isPopover: false,
-       label: "Features",
-       href: "/features",
-    },
-    {
-       isPopover: false,
-       label: "Enterprise",
-       href: "/enterprise",
-    },
-    {
-       isPopover: false,
-       label: "Live Demo",
-       href: "/demo",
-    },
-    {
-        isPopover: true,
-        label: "Community",
-        items: [
-            {
-                label: "Documentation",
-                description: "Everything you need to get started.",
-                link: "/docs/",
-                icon: DocumentsIcon,
-            },
-            {
-                label: "Forums",
-                description: "Join our growing community!",
-                link: "https://community.openpanel.org/",
-                icon: ContributeIcon,
-            },
-            {
-                label: "Translations",
-                description: "Help us improve OpenPanel!",
-                link: "https://github.com/stefanpejcic/openpanel-translations",
-                icon: HackathonsIcon,
-            },
-        ],
-    },
-  //  {
-   //     isPopover: true,
-  //      label: "Company",
-  //      items: [
-   //         {
-  //              label: "About Us",
-  //              description: "Team & company information.",
-       //         link: "/about",
-    //            icon: AboutUsIcon,
-  //          },
-     //       {
-    //            label: "Become a Partner",
-      //          description: "Help us spread the word!",
-      //          link: "mailto:info@openpanel.co",
-      //          icon: StoreIcon,
-    //        },
-     //       {
-     //           label: "Meet OpenPanel",
-     //           description: "Call us for any questions",
-     //           link: "mailto:info@openpanel.co",
-     //           icon: MeetIcon,
-     //       },
-    //    ],
- //   },
+  {
+    isPopover: true,
+    label: "Resources",
+    items: [
+      {
+        label: "Documentation",
+        description: "Everything you need to get started.",
+        link: "/docs/",
+        icon: DocumentsIcon,
+      },
+      {
+        label: "How-to Guides",
+        description: "How to customize OpenPanel.",
+        link: "/docs/",
+        icon: TutorialIcon,
+      },
+      {
+        label: "Features",
+        description: "Discover OpenPanel features.",
+        link: "/features/",
+        icon: AwesomeIcon,
+      },
+      {
+        label: "Changelog",
+        description: "New features and bug fixes.",
+        link: "/changelog/",
+        icon: IntegrationsIcon,
+      },
+      {
+        label: "Forums",
+        description: "Join our growing community!",
+        link: "https://community.openpanel.org/",
+        icon: ContributeIcon,
+      },
+      {
+        label: "Blog",
+        description: "Articles about Linux and server stuff.",
+        link: "/blog",
+        icon: BlogIcon,
+      },
+    ],
+  },
+  {
+    isPopover: false,
+    label: "Live Demo",
+    href: "/demo",
+    icon: NewBadgeIcon,
+  },
+  {
+    isPopover: false,
+    label: "Install",
+    href: "/install",
+  },
+  {
+    isPopover: true,
+    label: "Enterprise",
+    items: [
+      {
+        label: "OpenPanel Enterprise",
+        description: "Fixed pricing at 14.95€ monthly.",
+        link: "/enterprise/",
+        icon: StoreIcon,
+      },
+      {
+        label: "Hosting Providers",
+        description: "Hosting providers ❤️ OpenPanel.",
+        link: "/hosting-providers",
+        icon: ExamplesIcon,
+      },
+      {
+        label: "Contact us",
+        description: "Call us for any questions",
+        link: "mailto:info@openpanel.com",
+        icon: MeetIcon,
+      },
+    ],
+  },
 ];
