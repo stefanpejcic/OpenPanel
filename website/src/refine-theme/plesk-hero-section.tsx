e06d878cafb4fcb2c804fcc72af4080acd4e8815
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-22 13:14:19 +0200

    Create plesk-hero-section.tsx

---
+++ website/src/refine-theme/plesk-hero-section.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import clsx from "clsx";
+import { EnterpriseGetInTouchButton } from "./enterprise-get-in-touch-button";
+import { CommonThemedImage } from "./common-themed-image";
+
+export const EnterpriseHeroSection = ({
+    className,
+}: {
+    className?: string;
+}) => {
+    return (
+        <div
+            className={clsx(
+                "flex flex-col",
+                "landing-md:grid landing-md:grid-cols-12",
+                "not-prose",
+                className,
+            )}
+        >
+            <div className={clsx("flex flex-col", "col-start-1 col-end-8")}>
+                <h1
+                    className={clsx(
+                        "max-w-xl landing-md:max-w-[408px] landing-lg:max-w-non landing-lg:whitespace-nowrap",
+                        "text-[32px] leading-[40px] landing-sm:text-[56px] landing-sm:leading-[72px]",
+                        "tracking-tight",
+                        "text-start",
+                        "pl-4 landing-sm:pl-6 landing-md:pl-10",
+                        "dark:text-gray-0 text-gray-900",
+                        "landing-lg:pt-8",
+                    )}
+                >
+                    OpenPanel -{" "}
+                    <span
+                        className={clsx(
+                            "font-semibold",
+                            "dark:text-refine-cyan-alt dark:drop-shadow-[0_0_30px_rgba(71,235,235,0.25)]",
+                            "text-refine-blue drop-shadow-[0_0_30px_rgba(51,51,255,0.3)]",
+                        )}
+                    >
+                        Plesk
+                    </span>{" "}
+                    Alternative
+                </h1>
+                <p
+                    className={clsx(
+                        "max-w-[446px]",
+                        "mt-6",
+                        "pl-4 landing-sm:pl-6 landing-md:pl-10",
+                        "dark:text-gray-400 text-gray-600",
+                    )}
+                >
+                    OpenPanel Enterprise Edition provides robust user isolation and management features, designed for web hosting providers, all at a fixed price.{" "}
+                </p>
+                <EnterpriseGetInTouchButton
+                    className={clsx(
+                        "pl-4 landing-sm:pl-6 landing-md:pl-10",
+                        "mt-6 landing-lg:mt-16",
+                    )}
+                />
+            </div>
+            <div
+                className={clsx(
+                    "flex",
+                    "justify-end",
+                    "col-start-8",
+                    "col-end-13",
+                    "mt-12 landing-sm:mt-16 landing-md:mt-0",
+                )}
+            >
+                <CommonThemedImage
+                    className={clsx(
+                        "landing-md:h-[360px] landing-md:w-[326px]",
+                        "landing-md:h-[360px] landing-md:w-[326px]",
+                    )}
+                    srcDark="/img/hero.png"
+                    srcLight="/img/hero.png"
+                    alt="openpanel enterprise image"
+                />
+            </div>
+        </div>
+    );
+};
