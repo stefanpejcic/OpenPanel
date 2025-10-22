d37b77aee972432b1bcb5be5dec957db39c0ae58
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-22 13:56:04 +0200

    Update UPDATE.sh

--- version/1.6.4/UPDATE.sh
+++ version/1.6.4/UPDATE.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Updating default template..."
-default_file="/etc/openpanel/docker/compose/nodejs.yml"
+echo "Updating default NodeJS template..."
+default_node_file="/etc/openpanel/docker/compose/nodejs.yml"
 
-if [ -f "$default_file" ]; then
-    cp "$default_file" "$default_file.bak"
-    sed -i 's/_NODE_INSTALL/_NODE_REQUIREMENTS/g' "$default_file"
-    sed -i 's/_PY_INSTALL/_PY_REQUIREMENTS/g' "$default_file"
-    echo " - Updated $default_file (backup: $default_file.bak)"
+if [ -f "$default_node_file" ]; then
+    cp "$default_node_file" "$default_node_file.bak"
+    sed -i 's/_NODE_INSTALL/_NODE_REQUIREMENTS/g' "$default_node_file"
+    echo " - Updated $default_node_file (backup: $default_node_file.bak)"
 else
-    echo " - Default template not found at $default_file"
+    echo " - Default template not found at $default_node_file"
 fi
+
 
 echo "[*] Updating user docker-compose.yml files..."
 for f in /home/*/docker-compose.yml; do
