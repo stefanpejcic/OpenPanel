ebdea9f75ccbaa1d25c416d8fdc67060d3d2eb7f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-24 11:08:25 +0200

    5.30 based on admin.sh
    https://github.com/stefanpejcic/opencli/edit/main/admin.sh

--- install.sh
+++ install.sh
@@ -1555,10 +1555,10 @@ create_admin_and_show_logins_success_message() {
     fi
 
 	if [ "$SET_ADMIN_PASSWORD" = true ]; then
-	    if [[ "$custom_password" =~ ^[A-Za-z0-9]{5,16}$ ]]; then
+	    if [[ "$custom_password" =~ ^[A-Za-z0-9]{5,30}$ ]]; then
 	        new_password="${custom_password}"
 	    else
-	        echo "Warning: provided password is invalid (must be alphanumeric and 5–16 characters). Generating a secure password."
+	        echo "Warning: provided password is invalid (must be alphanumeric and 5–30 characters). Generating a secure password."
 	        new_password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
 	    fi
 	else
