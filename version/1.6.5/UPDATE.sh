bda485cac568d09118ee497ca27d534d125932a5
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-24 17:09:08 +0200

    Update UPDATE.sh

--- version/1.6.5/UPDATE.sh
+++ version/1.6.5/UPDATE.sh
@@ -2,3 +2,8 @@
 
 #wget -O /etc/openpanel/docker/compose/nodejs.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/nodejs.yml
 #wget -O /etc/openpanel/docker/compose/python.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/python.yml
+
+
+CONFIG_FILE="/etc/openpanel/openadmin/config/notifications.ini"
+LINE="dns=yes"
+grep -qxF "$LINE" "$CONFIG_FILE" || echo "$LINE" >> "$CONFIG_FILE"
