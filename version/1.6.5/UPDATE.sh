c52cc3926b9b439a07f99407792656aba393eb7d
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-24 14:13:21 +0200

    Update UPDATE.sh

--- version/1.6.5/UPDATE.sh
+++ version/1.6.5/UPDATE.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -O /etc/openpanel/docker/compose/nodejs.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/nodejs.yml
+#wget -O /etc/openpanel/docker/compose/nodejs.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/nodejs.yml
+#wget -O /etc/openpanel/docker/compose/python.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/python.yml
