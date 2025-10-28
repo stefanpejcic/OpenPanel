d6231a8f9762580fc96fc27302f6c350c0ee5117
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-28 12:04:32 +0100

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -21,7 +21,32 @@ const siteConfig = {
     organizationName: "stefanpejcic",
     trailingSlash: true,
     favicon: "img/favicon.svg",
-    scripts: ["https://platform.twitter.com/widgets.js"],
+  scripts: [
+    'https://platform.twitter.com/widgets.js',
+    {
+      id: 'linkedin-insight-init',
+      type: 'text/javascript',
+      content: `
+        _linkedin_partner_id = "8893345";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+      `,
+    },
+    {
+      id: 'linkedin-insight-loader',
+      type: 'text/javascript',
+      content: `
+        (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);
+        })(window.lintrk);
+      `,
+    },
     presets: [
         [
             "@docusaurus/preset-classic",
