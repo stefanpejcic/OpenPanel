ad4e86bfdc520f415739c3c1bd9b7f04fdb5e3e9
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2025-10-29 14:33:06 +0100

    Update UPDATE.sh

--- version/1.6.7/UPDATE.sh
+++ version/1.6.7/UPDATE.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget -O /etc/openpanel/docker/compose/1.0/.env https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/1.0/.env
+
 for file in /home/*/.env; do
     [ -f "$file" ] || continue
     sed -i 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' "$file"
