b1ca37b343f6824eef76c96592eda983ac050495
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2025-10-29 14:59:41 +0100

    Update UPDATE.sh

--- version/1.6.7/UPDATE.sh
+++ version/1.6.7/UPDATE.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+
+# Default template
 sed -i -e 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' \
-       -e 's/^PGADMIN_PASS="\([^"]*\)"/PGADMIN_PASS=\1/' \
+       -e 's/^PGADMIN_PASS="\([^"]*\)"/PGADMIN_PW=\1/' \
+       -e 's/^PGADMIN_PW="\([^"]*\)"/PGADMIN_PW=\1/' \
        /etc/openpanel/docker/compose/1.0/.env
 
+# Existing users
 for file in /home/*/.env; do
     [ -f "$file" ] || continue
-    sed -i 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' "$file"
-    sed -i 's/^PGADMIN_PASS="\([^"]*\)"/PGADMIN_PASS=\1/' "$file"
+    sed -i -e 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' \
+           -e 's/^PGADMIN_PASS="\([^"]*\)"/PGADMIN_PW=\1/' \
+           -e 's/^PGADMIN_PW="\([^"]*\)"/PGADMIN_PW=\1/' \
+           "$file"
 done
