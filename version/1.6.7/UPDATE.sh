c30cfa7258e1eb442c5ebda27301e1b465748d36
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-05 13:25:15 +0100

    Update UPDATE.sh

--- version/1.6.7/UPDATE.sh
+++ version/1.6.7/UPDATE.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo
+echo "Applying fix for #746"
 
-# Default template
 sed -i -e 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' \
        -e 's/^PGADMIN_PASS="\([^"]*\)"/PGADMIN_PW=\1/' \
        -e 's/^PGADMIN_PW="\([^"]*\)"/PGADMIN_PW=\1/' \
        /etc/openpanel/docker/compose/1.0/.env
 
-# Existing users
 for file in /home/*/.env; do
     [ -f "$file" ] || continue
     sed -i -e 's/^PGADMIN_MAIL="\([^"]*\)"/PGADMIN_MAIL=\1/' \
@@ -16,16 +16,12 @@ for file in /home/*/.env; do
            "$file"
 done
 
+echo
+echo "Removing server-motd command from crons"
 CRON_FILE="/etc/cron.d/openpanel"
-BACKUP_FILE="/etc/cron.d/openpanel.bak_$(date +%Y%m%d%H%M%S)"
 
-if [[ ! -f "$CRON_FILE" ]]; then
-    echo "Error: $CRON_FILE not found."
-    exit 1
+if [[ -f "$CRON_FILE" ]]; then
+    sed -i '/server-motd/d' "$CRON_FILE"
 fi
-
-cp "$CRON_FILE" "$BACKUP_FILE"
-echo "Backup created at: $BACKUP_FILE"
-sed -i '/server-motd/d' "$CRON_FILE"
 
 
