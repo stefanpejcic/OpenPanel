93004ee6f8aff73f25b7b4589e9dc0a02edbe3bd
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-13 15:51:31 +0100

    Update UPDATE.sh

--- version/1.6.8/UPDATE.sh
+++ version/1.6.8/UPDATE.sh
@@ -4,18 +4,16 @@ echo
 echo "📥 Updating features to include Mautic (BETA)"
 wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
 
+echo
+echo "Updating OpenPanel configuraiton to show errors regardless of dev_mode setting"
+wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
 
 set -euo pipefail
 
+echo
+echo "Adding configuration option for SSH logins on 'OpenAdmin > Notifications'."
+
 FILE="/etc/openpanel/openadmin/config/notifications.ini"
-BACKUP="${FILE}.$(date +%Y%m%d%H%M%S).bak"
-
-if [ ! -f "$FILE" ]; then
-  echo "Error: file does not exist: $FILE" >&2
-  exit 2
-fi
-
-cp -p "$FILE" "$BACKUP"
 
 if grep -xq 'ssh=yes' "$FILE"; then
   echo "ssh=yes already present. No changes needed."
