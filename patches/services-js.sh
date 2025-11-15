77cd93d7e4aa7e90147d1d577f6d7492e9a8896c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-15 22:08:41 +0100

    Update and rename services_js.sh to services-js.sh

--- patches/services-js.sh
+++ patches/services-js.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# services-js.sh
+#
+# Description:
+# This patch fixes the 500 error on user pages when Services module is enabled.
+# It is safe to run multiple times.
+#
+# Usage:
+# This script is intended to be run via opencli patch:
+#   opencli patch fservices-js
+#
 wget -O /tmp/file.html https://gist.githubusercontent.com/stefanpejcic/33b8c802d8aa91eeecfe39c5c0519124/raw/aeb59f090d8f61c7d18a47fb1534f9878d0a4bba/file.html
+
 docker cp /tmp/file.html openpaanel:/templates/partials/_service_js.html
+
 docker restart openpanel
