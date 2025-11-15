b3e4c4b6b9a04c6fd1ff49b8d063cabf61a6a070
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-15 22:10:41 +0100

    Update and rename services-js.sh to services.sh

--- patches/services.sh
+++ patches/services.sh
@@ -5,6 +5,6 @@
 
 wget -O /tmp/file.html https://gist.githubusercontent.com/stefanpejcic/33b8c802d8aa91eeecfe39c5c0519124/raw/aeb59f090d8f61c7d18a47fb1534f9878d0a4bba/file.html
 
-docker cp /tmp/file.html openpaanel:/templates/partials/_service_js.html
+docker cp /tmp/file.html openpanel:/templates/partials/_service_js.html
 
 docker restart openpanel
