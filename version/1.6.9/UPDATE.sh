2f723d6862d867bf22307d8d53faeba9ede8bdea
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-16 00:54:09 +0100

    Update UPDATE.sh

--- version/1.6.9/UPDATE.sh
+++ version/1.6.9/UPDATE.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo
+echo "Updating OpenPanel configuration to show errors regardless of dev_mode setting"
+wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
+
+echo
 echo "Updating WP-CLI"
 wget -O /etc/openpanel/wordpress/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
