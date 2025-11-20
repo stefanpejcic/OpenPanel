b1df775c419ba35609fcbe95622f4167fa39b948
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-20 14:46:16 +0100

    https://github.com/stefanpejcic/OpenPanel/issues/777#issuecomment-3558146078
    https://github.com/stefanpejcic/OpenPanel/issues/777#issuecomment-3558146078

--- version/1.7.0/UPDATE.sh
+++ version/1.7.0/UPDATE.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+echo 
+echo "Updating service file to show timestamp in docker log for openpanel service.."
+wget -o /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
+docker restart openpanel
+
 echo 
 echo "Disabling CoreRuleSet REQUEST-941-APPLICATION-ATTACK-XSS.conf"
 mv /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf.disabled
@@ -8,7 +14,6 @@ mv /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.con
 echo 
 echo "Adding 'ssl' module for Community edition.."
 wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
-
 
 echo 
 echo "Increasing activity_items_per_page value from 25 to 100"
