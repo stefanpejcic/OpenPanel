3b0f35646fed85a17eb00722b2216f42a6589919
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-21 10:32:32 +0100

    Update UPDATE.sh

--- version/1.7.1/UPDATE.sh
+++ version/1.7.1/UPDATE.sh
@@ -5,3 +5,9 @@ echo
 echo "Upgrading ConfigServer Firewall & Security to Sentinel Firewall"
 echo "https://sentinelfirewall.org/"
 bash <(curl -sSL https://raw.githubusercontent.com/sentinelfirewall/sentinel/refs/heads/main/public/upgrade.sh)
+
+echo
+echo "Downloading WordPress .htaccess templates for Apache and Litespeed webservers.."
+mkdir -p /etc/openpanel/wordpress/htaccess/
+wget -O /etc/openpanel/wordpress/htaccess/litespeed.htaccess https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/wordpress/htaccess/litespeed.htaccess
+wget -O /etc/openpanel/wordpress/htaccess/apache.htaccess https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/wordpress/htaccess/apache.htaccess
