1e4b4bd61ab7e419cb35fe6863218fabbb041d1c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-22 23:17:16 +0100

    Update UPDATE.sh

--- version/1.7.1/UPDATE.sh
+++ version/1.7.1/UPDATE.sh
@@ -11,3 +11,22 @@ echo "Downloading WordPress .htaccess templates for Apache and Litespeed webserv
 mkdir -p /etc/openpanel/wordpress/htaccess/
 wget -O /etc/openpanel/wordpress/htaccess/litespeed.htaccess https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/wordpress/htaccess/litespeed.htaccess
 wget -O /etc/openpanel/wordpress/htaccess/apache.htaccess https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/wordpress/htaccess/apache.htaccess
+
+
+
+echo
+echo "Generating secret key for OpenAdmin.."
+SECRET_FILE="/etc/openpanel/openadmin/secret.key"
+if [ ! -f "$SECRET_FILE" ]; then
+    openssl rand -hex 32 > "$SECRET_FILE"
+    chmod 600 "$SECRET_FILE"
+fi
+
+
+echo
+echo "Generating secret key for OpenPanel.."
+SECRET_FILE="/etc/openpanel/openpanel/secret.key"
+if [ ! -f "$SECRET_FILE" ]; then
+    openssl rand -hex 32 > "$SECRET_FILE"
+    chmod 600 "$SECRET_FILE"
+fi
