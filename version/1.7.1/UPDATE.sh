f94b252ba4451b54d22650bf5d9418cd0e7ed60d
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-25 15:28:56 +0100

    Update UPDATE.sh

--- version/1.7.1/UPDATE.sh
+++ version/1.7.1/UPDATE.sh
@@ -1,10 +1,10 @@
 #!/bin/badh
 
 # https://sentinelfirewall.org/
-echo
-echo "Upgrading ConfigServer Firewall & Security to Sentinel Firewall"
-echo "https://sentinelfirewall.org/"
-bash <(curl -sSL https://raw.githubusercontent.com/sentinelfirewall/sentinel/refs/heads/main/public/upgrade.sh)
+#echo
+#echo "Upgrading ConfigServer Firewall & Security to Sentinel Firewall"
+#echo "https://sentinelfirewall.org/"
+#bash <(curl -sSL https://raw.githubusercontent.com/sentinelfirewall/sentinel/refs/heads/main/public/upgrade.sh)
 
 echo
 echo "Downloading WordPress .htaccess templates for Apache and Litespeed webservers.."
