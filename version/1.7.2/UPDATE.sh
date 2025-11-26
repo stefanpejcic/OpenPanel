bb98fd8a1a5d4c6357b89528859f1876cf0b5b35
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-26 11:48:35 +0100

    Update UPDATE.sh

--- version/1.7.2/UPDATE.sh
+++ version/1.7.2/UPDATE.sh
@@ -3,3 +3,6 @@
 echo 
 echo "Updating service logs for OpenAdmin Log Viewer.."
 wget -O /etc/openpanel/openadmin/config/log_paths.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/log_paths.json
+
+
+mv /etc/openpanel/openadmin/config/terms_temporary_off /etc/openpanel/openadmin/config/terms.md
