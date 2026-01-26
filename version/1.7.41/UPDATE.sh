3a02d0a25a99cf681034cf958579ee194693f395
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-01-26 12:58:47 +0100

    Create UPDATE.sh

---
+++ version/1.7.41/UPDATE.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 
+wget -O /etc/openpanel/openadmin/config/shortcuts.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/shortcuts.json
