f6007efab84abc252507ed5f79741b7976ae0212
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-01-29 15:07:41 +0100

    Update UPDATE.sh

--- version/1.7.41/UPDATE.sh
+++ version/1.7.41/UPDATE.sh
@@ -7,3 +7,7 @@ wget -O /etc/openpanel/openadmin/config/shortcuts.json https://raw.githubusercon
 # https://community.openpanel.org/d/241-caddy-failing-after-adding-a-domain-waf-update-issue
 echo
 mv /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf.disabled /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf && docker restart caddy
+
+
+echo 
+wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json
