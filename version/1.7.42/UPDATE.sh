06673b3415c8e386a604f43b8e312f4478dd6f03
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-06 21:46:17 +0100

    Update UPDATE.sh

--- version/1.7.42/UPDATE.sh
+++ version/1.7.42/UPDATE.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+echo
+echo "Updating existing plans in database to add a new column for 'max_email_quota'.."
+timeout 10 mysql -e "
+SELECT COUNT(*) INTO @exists
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_schema = 'panel'
+  AND table_name = 'plans'
+  AND column_name = 'max_email_quota';
+
+SET @sql = IF(@exists = 0,
+  'ALTER TABLE plans ADD COLUMN max_email_quota TEXT NOT NULL; UPDATE plans SET max_email_quota = \"0\";',
+  'SELECT \"Column already exists\";'
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+"
+
+
+
+
+echo
+echo "Checking default template and .env files for users for missing G in _RAM keys.."
 (
   find /home -maxdepth 2 -type f -name ".env" -path "/home/*/.env"
   echo "/etc/openpanel/docker/compose/1.0/.env"
@@ -26,7 +50,8 @@ done
 
 
 
-
+echo
+echo "Adding PageSpeed API key for WP Manager.."
 file="/etc/openpanel/openpanel/service/pagespeed.api"
 key="AIzaSyDow0GLE7N5gcZXa72tpqIvIaJtn1bDtsk"
 
@@ -37,8 +62,9 @@ fi
 
 
 
+echo
+echo "Updating openpanel.config file to add 'filemanager_buttons_style' setting.."
 CONFIG="/etc/openpanel/openpanel/conf/openpanel.config"
-
 cp -a "$CONFIG" /tmp/openpanel.config_1.7.42.bak
 
 if ! grep -q "^filemanager_buttons_style=" "$CONFIG"; then
@@ -66,6 +92,8 @@ else
   echo "filemanager_buttons_style=classic already exists — skipping"
 fi
 
+echo
+echo "Updating openpanel.config file to add 'mysql_startup_time' 'mysql_import_max_size_gb' 'mysql_restricted_usernames' 'mysql_restricted_databases' settings.."
 if ! grep -q "^\[DATABASES\]" "$CONFIG"; then
   cat >> "$CONFIG" << 'EOF'
 
