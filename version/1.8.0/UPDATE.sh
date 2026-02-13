f5bbeabf0e3ade09f46abb84fd978640fe7e3f6a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-13 13:25:21 +0100

    Update UPDATE.sh

--- version/1.8.0/UPDATE.sh
+++ version/1.8.0/UPDATE.sh
@@ -3,4 +3,14 @@
 
 # MAJOR
 
+echo
+echo "Updating public_suffix_list file for new TLDs.."
 wget -o /etc/openpanel/openpanel/conf/public_suffix_list.dat https://publicsuffix.org/list/public_suffix_list.dat
+
+
+echo
+echo "Updating admin panel service to lower resource consumption.."
+wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+systemctl daemon-reload
+systemctl restart  admin
