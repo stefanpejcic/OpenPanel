418240951ee3f5b838df46276ef9dd0bdb5f3c17
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-21 00:16:09 +0100

    Update UPDATE.sh

--- version/1.7.44/UPDATE.sh
+++ version/1.7.44/UPDATE.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo
-wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+opencli config update dev_mode off
 
-wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
-
-
-wget -O /etc/systemd/system/admin.service https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/openadmin.service
-
-sed -i '/interval 1h/d' /etc/openpanel/caddy/Caddyfile && docker --copntext=default restart caddy
+# services
+curl -L -o /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+curl -L -o /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
+curl -L -o /etc/systemd/system/admin.service https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/openadmin.service
 
 systemctl daemon-reload
+
+# caddy
+sed -i '/interval 1h/d' /etc/openpanel/caddy/Caddyfile && docker --copntext=default restart caddy
+
 
