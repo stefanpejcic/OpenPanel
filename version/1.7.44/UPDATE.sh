f272ecd73841cf74cada1843b8096026b2271984
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-20 19:51:04 +0100

    Update UPDATE.sh

--- version/1.7.44/UPDATE.sh
+++ version/1.7.44/UPDATE.sh
@@ -4,5 +4,7 @@ echo
 wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
 wget -O /etc/systemd/system/admin.service https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/openadmin.service
 
+sed -i '/interval 1h/d' /etc/openpanel/caddy/Caddyfile && docker --copntext=default restart caddy
+
 systemctl daemon-reload
 
