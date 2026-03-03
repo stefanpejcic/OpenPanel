0a67ca88d5efe4db7dc4fdc0f6afa26e95b047d8
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-03 11:03:18 +0100

    Update UPDATE.sh

--- version/1.7.45/UPDATE.sh
+++ version/1.7.45/UPDATE.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Applying patch for WP-CLI incorrect path on OpenLitespeed.."
 sed -i 's|/usr/bin/wp|/usr/local/bin/wp:ro|g' /etc/openpanel/docker/compose/1.0/docker-compose.yml
 
+for file in /home/*/docker-compose.yml; do
+    if [ -f "$file" ]; then
+        sed -i 's|/usr/bin/wp|/usr/local/bin/wp:ro|g' "$file"
+        echo "Updated: $file"
+    fi
+done
