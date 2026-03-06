6f8b89a11495472be247b99917ec01e69df5d03f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-06 18:17:44 +0100

    Update UPDATE.sh

--- version/1.7.46/UPDATE.sh
+++ version/1.7.46/UPDATE.sh
@@ -3,3 +3,7 @@
 echo "Applying patch for WAF error starting Caddy.."
 sed -i '/^SecRequestBodyJsonDepthLimit/ s/^/#/' /etc/openpanel/caddy/coraza_rules.conf
 docker --context=default restart caddy
+
+
+echo "Updating MOTD for admins to support custom OpenAdmin port.."
+wget -O /etc/openpanel/ssh/admin_welcome.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/ssh/admin_welcome.sh
