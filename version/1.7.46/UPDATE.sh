ed3e9a100ecad7a7a28f2a844a2c8dd5447ba419
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-06 19:16:08 +0100

    Update UPDATE.sh

--- version/1.7.46/UPDATE.sh
+++ version/1.7.46/UPDATE.sh
@@ -7,3 +7,14 @@ docker --context=default restart caddy
 
 echo "Updating MOTD for admins to support custom OpenAdmin port.."
 wget -O /etc/openpanel/ssh/admin_welcome.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/ssh/admin_welcome.sh
+
+
+echo "Adding support for Webhooks on OpenAdmin Notifications.."
+file="/etc/openpanel/openadmin/config/notifications.ini"
+
+if ! grep -Fxq "webhook_url" "$file"; then
+    echo "webhook_url=" >> "$file"
+    echo "webhook_url option appended to $file."
+else
+    echo "webhook_url option already exists in $file."
+fi
