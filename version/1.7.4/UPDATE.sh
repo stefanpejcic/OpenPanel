8d702d371e6523861eeb1dc54a2429f40364dc8d
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-13 21:54:29 +0100

    Update UPDATE.sh

--- version/1.7.4/UPDATE.sh
+++ version/1.7.4/UPDATE.sh
@@ -2,3 +2,6 @@
 
 # https://github.com/stefanpejcic/OpenPanel/issues/806
 sed -i 's/^reseller=no$/reseller=yes/' /etc/openpanel/openadmin/config/admin.ini
+
+
+sudo curl -sSL -o /etc/openpanel/caddy/check.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/caddy/check.conf
