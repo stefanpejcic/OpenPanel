c7dac45443add6600b5f6925bacb85e0ce9f0686
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-17 21:05:39 +0100

    Update UPDATE.sh

--- version/1.8.0/UPDATE.sh
+++ version/1.8.0/UPDATE.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo
+echo "Adding max_hourly_email column to plans.."
+timeout 3 mysql -e "ALTER TABLE plans ADD COLUMN max_hourly_email INT UNSIGNED NOT NULL DEFAULT 0;"
 
 # MAJOR
 
