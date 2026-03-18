18c911f78ecd476a9f78014e378d480470cd9b89
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-18 13:45:22 +0100

    Update UPDATE.sh

--- version/1.7.47/UPDATE.sh
+++ version/1.7.47/UPDATE.sh
@@ -3,3 +3,11 @@
 echo
 echo "Adding max_hourly_email column to plans.."
 timeout 3 mysql -e "ALTER TABLE plans ADD COLUMN max_hourly_email INT UNSIGNED NOT NULL DEFAULT 0;"
+
+echo
+output=$(opencli license key)
+
+if [[ "$output" == enterprise* ]]; then
+    echo "Adding rate-imiting for email accounts.."
+    timeout 30 opencli email-server postfwd enable
+fi
