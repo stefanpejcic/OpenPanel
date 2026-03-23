c686a141319e6ecfdc910e372d30ea313fd56533
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-23 13:56:11 +0100

    Update UPDATE.sh

--- version/1.7.48/UPDATE.sh
+++ version/1.7.48/UPDATE.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo
+echo "Installing slask-sock for OpenAdmin > Advanced > Web Terminal"
 source /usr/local/admin/venv/bin/activate && pip install flask-sock && deactivate
 service admin restart
