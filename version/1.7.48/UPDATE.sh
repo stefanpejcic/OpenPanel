713cb65f8de8e2454a54ada0b322e851fade2a2a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-23 18:20:48 +0100

    Update UPDATE.sh

--- version/1.7.48/UPDATE.sh
+++ version/1.7.48/UPDATE.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo
-echo "Installing slask-sock for OpenAdmin > Advanced > Web Terminal"
+echo "Installing flask-sock for OpenAdmin > Advanced > Web Terminal"
 source /usr/local/admin/venv/bin/activate && pip install flask-sock && deactivate
 service admin restart
+
+
+echo
+echo ""
+sed -i.bak 's|/run/user/:/hostfs/run/user/:ro|/run/user/:/hostfs/run/user/:shared,rslave|g' /root/docker-compose.yml
+cd /root && docker --context=default compose down openpanel && docker --context=default compose up -d openpanel
