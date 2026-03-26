7b77906193151cbadc8cb8fb450936431a33d974
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-26 18:54:06 +0100

    Update UPDATE.sh

--- version/1.7.50/UPDATE.sh
+++ version/1.7.50/UPDATE.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo 
+echo "Adding patch for FTP service"
+yes yes | opencli patch 896
+
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
 docker restart openpanel
