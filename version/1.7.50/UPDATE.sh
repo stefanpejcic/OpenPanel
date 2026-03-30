6bbe1e623a0b3290c752f7a48860aae191476d36
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-30 15:58:43 +0200

    Update UPDATE.sh

--- version/1.7.50/UPDATE.sh
+++ version/1.7.50/UPDATE.sh
@@ -5,6 +5,11 @@ echo "Adding patch for FTP service"
 yes yes | opencli patch 896
 
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
-docker restart openpanel
 
 ln -s /usr/local/mail/openmail/mailserver.env /usr/local/mail/openmail/.env
+
+echo
+echo "Setting local screenshots.."
+sed -i 's/^\(screenshots=\).*$/\1local/' /etc/openpanel/openpanel/conf/openpanel.config
+
+docker restart openpanel
