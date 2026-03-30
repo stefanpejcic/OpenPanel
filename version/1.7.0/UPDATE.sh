79705091d08f5696ed9c9d2e5a5fc755b493e608
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-30 15:57:40 +0200

    Update UPDATE.sh

--- version/1.7.0/UPDATE.sh
+++ version/1.7.0/UPDATE.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
-
 echo 
 echo "Updating service file to show timestamp in docker log for openpanel service.."
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
-docker restart openpanel
 
 echo 
 echo "Disabling CoreRuleSet REQUEST-941-APPLICATION-ATTACK-XSS.conf"
 mv /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf /etc/openpanel/caddy/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf.disabled
-
 
 echo 
 echo "Adding 'ssl' module for Community edition.."
@@ -18,3 +14,11 @@ wget -O /etc/openpanel/openadmin/config/features.json https://raw.githubusercont
 echo 
 echo "Increasing activity_items_per_page value from 25 to 100"
 opencli config update activity_items_per_page 100
+
+echo
+echo "Setting local screenshots.."
+sed -i 's/^\(screenshots=\).*$/\1local/' /etc/openpanel/openpanel/conf/openpanel.config
+
+
+
+docker restart openpanel
