328d805fa3ce5e9c179a6f647a74cadb0e587432
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-06 20:42:27 +0200

    Update UPDATE.sh

--- version/1.7.52/UPDATE.sh
+++ version/1.7.52/UPDATE.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 opencli config update screenshots local
+
+
+wget -O /etc/openpanel/docker/daemon/rootless.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/daemon/rootless.json
