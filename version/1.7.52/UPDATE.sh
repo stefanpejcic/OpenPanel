e9a9378a85b83052d2e243b1da01946180486aa9
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-08 14:01:32 +0200

    Update UPDATE.sh

--- version/1.7.52/UPDATE.sh
+++ version/1.7.52/UPDATE.sh
@@ -4,3 +4,5 @@ opencli config update screenshots local
 
 
 wget -O /etc/openpanel/docker/daemon/rootless.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/daemon/rootless.json
+
+sed -i '/^SecRxPreFilter/s/^/#/' /etc/openpanel/caddy/coraza_rules.conf && docker --context=default restart caddy
