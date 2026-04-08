ca8bd5f82e952f9dabd0874ffe058746e89afc50
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-08 14:22:40 +0200

    Update UPDATE.sh

--- version/1.7.52/UPDATE.sh
+++ version/1.7.52/UPDATE.sh
@@ -2,7 +2,16 @@
 
 opencli config update screenshots local
 
-
 wget -O /etc/openpanel/docker/daemon/rootless.json https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/daemon/rootless.json
 
 sed -i '/^SecRxPreFilter/s/^/#/' /etc/openpanel/caddy/coraza_rules.conf && docker --context=default restart caddy
+
+timeout 5  mysql -uroot -e "SET GLOBAL event_scheduler = ON;"
+
+timeout 5 mysql -uroot <<EOF
+CREATE EVENT IF NOT EXISTS cleanup_expired_sessions
+ON SCHEDULE EVERY 15 MINUTE
+DO
+  DELETE FROM active_sessions
+  WHERE expires_at < NOW();
+EOF
