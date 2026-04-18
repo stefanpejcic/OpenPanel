5c526b3777260399fedd2d610b65f368f2752cbd
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-18 16:22:45 +0200

    Update UPDATE.sh

--- version/1.7.55/UPDATE.sh
+++ version/1.7.55/UPDATE.sh
@@ -3,3 +3,8 @@
 
 wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
 wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
+
+
+
+echo "Adding patch for shortlived Let's Encrypt SSL certificates for IP address.."
+cd /root && docker compose down caddy && docker image rm openpanel/caddy-coraza && docker compose up -d caddy
