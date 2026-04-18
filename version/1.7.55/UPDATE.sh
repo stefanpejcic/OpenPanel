2910456b056ebcf7bcaa5915268eecdbf85ab74e
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-18 15:42:27 +0200

    Update UPDATE.sh

--- version/1.7.55/UPDATE.sh
+++ version/1.7.55/UPDATE.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+
+wget -O /etc/openpanel/openpanel/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openpanel/service/service.config.py
+wget -O /etc/openpanel/openadmin/service/service.config.py https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/service/service.config.py
