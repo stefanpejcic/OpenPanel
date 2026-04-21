7d947b4988db10fcb6f973e94dd20529d4e82d71
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-21 12:55:22 +0200

    Update UPDATE.sh

--- version/1.7.56/UPDATE.sh
+++ version/1.7.56/UPDATE.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 wget -O /etc/openpanel/ssh/admin_welcome.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/ssh/admin_welcome.sh && chmod +x /etc/profile.d/welcome.sh
+
+
+wget -O /etc/openpanel/wordpress/wp-cli.phar https://github.com/wp-cli/wp-cli/releases/download/v2.12.0/wp-cli-2.12.0.phar && chmod +x /etc/openpanel/wordpress/wp-cli.phar
