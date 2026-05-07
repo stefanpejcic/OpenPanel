c087a2fc01705c8c95a371ba2619cda54723e944
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-07 11:07:17 +0200

    Update UPDATE.sh

--- version/1.7.57/UPDATE.sh
+++ version/1.7.57/UPDATE.sh
@@ -2,3 +2,4 @@
 
 
 wget -O /etc/openpanel/caddy/check.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/caddy/check.conf
+wget -O /etc/openpanel/docker/dockerd-rootless-setuptool.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/dockerd-rootless-setuptool.sh ; chmod +x /etc/openpanel/docker/dockerd-rootless-setuptool.sh
