009302f7af3808fb34711e2f002f465b2939ad71
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-08 13:04:23 +0200

    Update UPDATE.sh

--- version/1.7.57/UPDATE.sh
+++ version/1.7.57/UPDATE.sh
@@ -3,7 +3,7 @@
 # update caddy check to issue www. SSL
 wget -O /etc/openpanel/caddy/check.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/caddy/check.conf
 
-# fixes bug on user-add for 4-6.May
+# fixes bug on user-add for 4-8.May
 wget -O /etc/openpanel/docker/dockerd-rootless-setuptool.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/dockerd-rootless-setuptool.sh ; chmod +x /etc/openpanel/docker/dockerd-rootless-setuptool.sh
 
 # add cron for updating du info
