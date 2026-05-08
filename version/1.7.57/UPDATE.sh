5c3d1a252bd8a0c5461248a83ffd7d9ebb0cec8d
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-08 12:09:34 +0200

    Update UPDATE.sh

--- version/1.7.57/UPDATE.sh
+++ version/1.7.57/UPDATE.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-
+# update caddy check to issue www. SSL
 wget -O /etc/openpanel/caddy/check.conf https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/caddy/check.conf
+
+# fixes bug on user-add for 4-6.May
 wget -O /etc/openpanel/docker/dockerd-rootless-setuptool.sh https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/dockerd-rootless-setuptool.sh ; chmod +x /etc/openpanel/docker/dockerd-rootless-setuptool.sh
+
+# add cron for updating du info
+QUOTA_LINE='*/5 * * * * root /usr/local/bin/opencli user-quota && echo "$(date) Collected disk and inodes usage for all users" >> /var/log/openpanel/admin/cron.log'
+
+if !grep -q "user-quota" "/etc/cron.d/openpanel"; then
+    sed -i "/^# STATISTICS/a ${QUOTA_LINE}" "/etc/cron.d/openpanel"
+fi
