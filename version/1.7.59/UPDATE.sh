a029fc77a40b9daa86ecd56738df45205412248f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-15 17:44:19 +0200

    Update UPDATE.sh

--- version/1.7.59/UPDATE.sh
+++ version/1.7.59/UPDATE.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+# created in 1.7.58 update
+if [ -f "/etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml" ]; then  
+  mv /etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml /tmp/docker-compose_backup_before_1.7.58_update.yml
+fi
