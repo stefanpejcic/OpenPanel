20c35800d21f159681984887dff6e66d368b6b67
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-17 23:20:35 +0200

    Update UPDATE.sh

--- version/1.7.59/UPDATE.sh
+++ version/1.7.59/UPDATE.sh
@@ -4,3 +4,9 @@
 if [ -f "/etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml" ]; then  
   mv /etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml /tmp/docker-compose_backup_before_1.7.58_update.yml
 fi
+
+if [ -f "/usr/local/mail/openmail/docker-data/dms/config/postfix-accounts_backup_before_1.7.58_update.cf" ]; then  
+  mv /usr/local/mail/openmail/docker-data/dms/config/postfix-accounts_backup_before_1.7.58_update.cf /tmp/postfix-accounts_backup_before_1.7.58_update.cf
+fi
+
+
