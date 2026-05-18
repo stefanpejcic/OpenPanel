bbeafe28046d8295b4d0f67a9f75adbcc733422b
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-18 12:40:49 +0200

    Update UPDATE.sh

--- version/1.7.59/UPDATE.sh
+++ version/1.7.59/UPDATE.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# fixes bug with autologin
+cd /usr/local/mail/openmail
+docker --context=default compose down roundcube
+rm -rf /usr/local/mail/openmail/roundcube-plugins/autologin.php
+wget -O "/usr/local/mail/openmail/roundcube-plugins/autologin.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/autologin.php
+docker --context=default compose up -d roundcube
+
+
+
 # created in 1.7.58 update
 if [ -f "/etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml" ]; then  
   mv /etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml /tmp/docker-compose_backup_before_1.7.58_update.yml
