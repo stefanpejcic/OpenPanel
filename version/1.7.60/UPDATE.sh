607f4f5dfd5d7f045c9bf3c3ad52bf6b628e92c5
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-21 19:35:16 +0200

    Update UPDATE.sh

--- version/1.7.60/UPDATE.sh
+++ version/1.7.60/UPDATE.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# created in 1.7.58 update
+if [ -f "/etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml" ]; then  
+  mv /etc/openpanel/docker/compose/1.0/docker-compose_backup_before_1.7.58_update.yml /tmp/docker-compose_backup_before_1.7.58_update.yml
+fi
+
+if [ -f "/usr/local/mail/openmail/docker-data/dms/config/postfix-accounts_backup_before_1.7.58_update.cf" ]; then  
+  mv /usr/local/mail/openmail/docker-data/dms/config/postfix-accounts_backup_before_1.7.58_update.cf /tmp/postfix-accounts_backup_before_1.7.58_update.cf
+fi
+
 # ENABLE GZIP
 DIR="/etc/openpanel/caddy/domains"
 mkdir -p /tmp/domain_backups_1.7.59
