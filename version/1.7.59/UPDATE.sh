6c915665764a4406b6e43188604c810b52c1fe2a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-02 18:01:56 +0200

    Update UPDATE.sh

--- version/1.7.59/UPDATE.sh
+++ version/1.7.59/UPDATE.sh
@@ -4,10 +4,10 @@
 cd /usr/local/mail/openmail
 docker --context=default compose down roundcube
 rm -rf /usr/local/mail/openmail/roundcube-plugins/autologin.php
+mkdir -p /usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/
 wget -O "/usr/local/mail/openmail/roundcube-plugins/autologin.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/autologin.php
 wget -O "/usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php
 docker --context=default compose up -d roundcube
-
 
 # fixes bug with ftp connections
 VSFTPD_FILE="/etc/openpanel/ftp/vsftpd.conf"
@@ -18,4 +18,3 @@ if [ -f "$VSFTPD_FILE" ]; then
     timeout 3 docker restart openadmin_ftp >/dev/null 2>&1
   fi
 fi
-
