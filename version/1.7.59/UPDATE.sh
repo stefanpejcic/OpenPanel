fcd460369f1a641b09133b1970dad2a4e0f8815b
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-03 21:43:22 +0200

    Update UPDATE.sh

--- version/1.7.59/UPDATE.sh
+++ version/1.7.59/UPDATE.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# fixes bug with autologin
-cd /usr/local/mail/openmail
-docker --context=default compose down roundcube
-rm -rf /usr/local/mail/openmail/roundcube-plugins/autologin.php
-mkdir -p /usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/
-wget -O "/usr/local/mail/openmail/roundcube-plugins/autologin.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/autologin.php
-wget -O "/usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php
-docker --context=default compose up -d roundcube
+# fixes bug with autologin to webmail on some installations
+SHOULD_START_AGAIN=false
+if [ -d "/usr/local/mail/openmail" ]; then
+  cd /usr/local/mail/openmail || exit 1
+
+  if docker --context=default ps --format '{{.Names}}' | grep -qx "roundcube"; then
+    SHOULD_START_AGAIN=true
+    timeout 5 docker --context=default compose down roundcube
+  fi
+
+  rm -f /usr/local/mail/openmail/roundcube-plugins/autologin.php
+  mkdir -p /usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/
+  wget -O "/usr/local/mail/openmail/roundcube-plugins/autologin.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/autologin.php
+  wget -O "/usr/local/mail/openmail/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php" https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/roundcube-plugins/dovecot_impersonate/dovecot_impersonate.php
+
+  if [ "$SHOULD_START_AGAIN" = true ]; then
+    timeout 5 docker --context=default compose up -d roundcube
+  fi
+fi
 
 # fixes bug with ftp connections
 VSFTPD_FILE="/etc/openpanel/ftp/vsftpd.conf"
@@ -15,6 +26,8 @@ LINE="setproctitle_enable=yes|"
 if [ -f "$VSFTPD_FILE" ]; then
   if ! grep -q "^setproctitle_enable=" "$VSFTPD_FILE"; then
     echo "$LINE" >> "$VSFTPD_FILE"
-    timeout 3 docker restart openadmin_ftp >/dev/null 2>&1
+    if docker --context=default ps --format '{{.Names}}' | grep -qx "openadmin_ftp"; then
+      timeout 5 docker --context=default restart openadmin_ftp >/dev/null 2>&1
+    fi
   fi
 fi
