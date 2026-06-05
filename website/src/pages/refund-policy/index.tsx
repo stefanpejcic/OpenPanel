d56518f485ccb92cf4e1c49be32c621d051e195c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-05 10:18:26 +0200

    Create index.tsx

---
+++ website/src/pages/refund-policy/index.tsx
@@ -0,0 +1,31 @@
+// Import React and necessary components from Docusaurus and other packages
+import React from 'react';
+import Head from '@docusaurus/Head';
+import { BlogFooter } from '@site/src/refine-theme/blog-footer';
+import { CommonHeader } from '@site/src/refine-theme/common-header';
+import { CommonLayout } from '@site/src/refine-theme/common-layout';
+import clsx from 'clsx';
+
+// Import the Markdown file as a React component
+import PrivacyContent from '@site/src/pages/privacy-policy/privacy.md';
+
+const PrivacyPolicy: React.FC = () => {
+    return (
+        <CommonLayout>
+            <Head title="Refund Policy | OpenPanel">
+                <html data-page="privacy_policy" data-customized="true" />
+            </Head>
+            <div className="refine-prose">
+                <CommonHeader hasSticky={true} />
+
+                <div className="flex-1 flex flex-col pt-8 lg:pt-16 pb-32 max-w-[800px] w-full mx-auto px-2">
+                    {/* Render the imported Markdown content as a component */}
+                    <PrivacyContent />
+                </div>
+                <BlogFooter />
+            </div>
+        </CommonLayout>
+    );
+};
+
+export default PrivacyPolicy;
