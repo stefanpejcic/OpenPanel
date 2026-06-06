873cb7b32211047d7ffbc565d964df3bf49ca730
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-06 12:40:52 +0200

    Update UPDATE.sh

--- version/1.7.61/UPDATE.sh
+++ version/1.7.61/UPDATE.sh
@@ -2,6 +2,7 @@
 
 for env_file in /home/*/docker-compose.yml; do
     user_dir="$(dirname "$env_file")"
+    rm -rf /home/$user_dir/pma.php
     timeout 5 docker --context="$user_dir" image rm phpmyadmin:latest > /dev/null 2>&1
 done
 
