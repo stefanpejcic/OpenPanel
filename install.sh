24bb9c539a5531d97f72a1bdd27c02378dc60ae7
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-09 13:32:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 # Usage:                   bash <(curl -sSL https://openpanel.org)
 # Author:                  Stefan Pejcic <stefan@pejcic.rs>
 # Created:                 11.07.2023
-# Last Modified:           08.06.2026
+# Last Modified:           09.06.2026
 ################################################################################
 # shellcheck disable=SC2015
 
