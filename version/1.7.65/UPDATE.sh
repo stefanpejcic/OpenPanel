71f5cbd2ee28d232905845ba03ad49cf286dd313
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-07-04 00:33:23 +0200

    Update UPDATE.sh

--- version/1.7.65/UPDATE.sh
+++ version/1.7.65/UPDATE.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 wget -O /usr/local/bin/opencli https://raw.githubusercontent.com/stefanpejcic/opencli/refs/heads/main/opencli && chmod +x /usr/local/bin/opencli
+
+
+CONFIG_FILE="/etc/openpanel/openpanel/conf/openpanel.config"
+if grep -q '^\[USERS\]' "$CONFIG_FILE" && ! grep -q '^password_strength=' "$CONFIG_FILE"; then
+  awk '
+  BEGIN {done=0}
+  /^\[USERS\]/ {
+    print
+    print "password_strength=50"
+    done=1
+    next
+  }
+  {print}
+  ' "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+fi
