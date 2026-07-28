21ec4ebb6a96a8a714696a734ab17fb31fc73e20
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-07-28 17:11:37 +0200

    Update PODMAN_INSTALL.sh

--- PODMAN_INSTALL.sh
+++ PODMAN_INSTALL.sh
@@ -9,7 +9,7 @@
 # Usage:                   bash <(curl -sSL https://openpanel.org/)
 # Author:                  Stefan Pejcic <stefan@pejcic.rs>
 # Created:                 11.07.2023
-# Last Modified:           24.07.2026
+# Last Modified:           28.07.2026
 ################################################################################
 # shellcheck disable=SC2015
 
