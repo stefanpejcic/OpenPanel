855282fbb30b440d12582c4012545d7ca5152183
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-03 12:17:28 +0200

    Update PODMAN_INSTALL.sh

--- PODMAN_INSTALL.sh
+++ PODMAN_INSTALL.sh
@@ -386,7 +386,6 @@ clone_repos() {
     [[ "$architecture" == "aarch64" ]] && admin_binary="openadmin-arm64"
     echo "Downloading openadmin to /usr/local/admin/$admin_binary"
     mkdir -p /usr/local/admin/
-    git clone --branch main --single-branch https://github.com/stefanpejcic/openadmin.git /usr/local/admin >/dev/null 2>&1
     curl -sSL "https://github.com/stefanpejcic/openadmin/releases/download/${PANEL_VERSION%%-beta}/$admin_binary" -o "/usr/local/admin/$admin_binary"
     [[ -f "/usr/local/admin/$admin_binary" ]] || die 1 "Failed to download OpenAdmin binary ${admin_binary} from Github."
     chmod +x "/usr/local/admin/$admin_binary"
