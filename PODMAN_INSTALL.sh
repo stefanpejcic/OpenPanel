f1bf602846ceedc2499ad8a5be37fffb8991cd12
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2026-08-07 13:52:15 +0200

    Change mariadb to mariadb-client in package lists

--- PODMAN_INSTALL.sh
+++ PODMAN_INSTALL.sh
@@ -338,21 +338,21 @@ install_packages() {
             ;;
         yum)
             run yum install -y dnf-plugins-core yum-utils epel-release
-            packages=(curl openssl cronie git tar dbus-user-session systemd dbus systemd-container quota uidmap iptables podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb sqlite3)
+            packages=(curl openssl cronie git tar dbus-user-session systemd dbus systemd-container quota uidmap iptables podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb-client sqlite3)
             ;;
         dnf)
             if [[ "$OS_ID" == "openeuler" ]]; then
                 run dnf install -y dnf-plugins-core yum-utils perl gcc tar
-                packages=(git tar curl openssl ncurses wget cronie systemd dbus systemd-container quota shadow-utils podman podman-compose crun netavark aardvark-dns passt slirp4netns fuse-overlayfs mariadb sqlite perl-Math-BigInt)
+                packages=(git tar curl openssl ncurses wget cronie systemd dbus systemd-container quota shadow-utils podman podman-compose crun netavark aardvark-dns passt slirp4netns fuse-overlayfs mariadb-client sqlite perl-Math-BigInt)
                 wait_for_pkg_lock
                 install_pkgs_batch "${packages[@]}"
                 return
             fi
             run dnf install -y yum-utils epel-release perl gcc
             if [[ -f /etc/fedora-release ]]; then
-                packages=(git tar openssl wget dbus-user-session systemd dbus systemd-container quota uidmap podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb sqlite perl-Math-BigInt)
+                packages=(git tar openssl wget dbus-user-session systemd dbus systemd-container quota uidmap podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb-client sqlite perl-Math-BigInt)
             else
-                packages=(git tar openssl ncurses wget systemd dbus systemd-container quota shadow-utils podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb sqlite perl-Math-BigInt)
+                packages=(git tar openssl ncurses wget systemd dbus systemd-container quota shadow-utils podman podman-compose crun netavark aardvark-dns slirp4netns passt fuse-overlayfs mariadb-client sqlite perl-Math-BigInt)
             fi
             ;;
     esac
