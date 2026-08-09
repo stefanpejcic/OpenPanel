f706daa2ef7a7f54b0c779daa9034f41d16301f1
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-09 15:40:00 +0200

    Update PODMAN_INSTALL.sh

--- PODMAN_INSTALL.sh
+++ PODMAN_INSTALL.sh
@@ -9,7 +9,7 @@
 # Usage:                   bash <(curl -sSL https://openpanel.org/)
 # Author:                  Stefan Pejcic <stefan@pejcic.rs>
 # Created:                 11.07.2023
-# Last Modified:           08.08.2026
+# Last Modified:           09.08.2026
 ################################################################################
 # shellcheck disable=SC2015
 
@@ -44,7 +44,7 @@ custom_password=""
 EMAIL=""
 license_key=""
 
-readonly DEFAULT_PANEL_VERSION="2.0.0"
+readonly DEFAULT_PANEL_VERSION="2.0.1"
 readonly CONTAINER_ENGINE="podman"
 readonly SHARED_STORE="/var/lib/containers/shared-storage"
 readonly ETC_DIR="/etc/openpanel/"
