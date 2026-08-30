4bccd0d253394abd079c7d4b404b64f5aa15dec1
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-30 07:01:42 +0200

    Update UPDATE.sh

--- UPDATES/2.0.3/UPDATE.sh
+++ UPDATES/2.0.3/UPDATE.sh
@@ -3,3 +3,6 @@
 # pm2 module is split
 panel_config="/etc/openpanel/openpanel/conf/openpanel.config"
 grep -q 'pm2' "$panel_config" && ! grep -qE 'python|nodejs|ruby' "$panel_config" && sed -i 's/pm2/python,nodejs,ruby/g' "$panel_config"
+
+# docker module is split
+grep -q 'docker' "$panel_config" && ! grep -qE 'terminal|change_image' "$panel_config" && sed -i 's/docker/docker,terminal,change_image/g' "$panel_config"
