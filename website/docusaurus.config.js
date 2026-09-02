6ee2e0cff7aff532d5cd26f333f937bc82c79dc8
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-02 07:37:54 +0200

    Update Docusaurus version from 2.0.3 to 2.0.4

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,7 +60,7 @@ const siteConfig = {
                               process.env.DISABLE_VERSIONING === "true",
                           versions: {
                               current: {
-                                  label: "2.0.3",
+                                  label: "2.0.4",
                               },
                           },
                           lastVersion: "current",
