d778c4ee061b0a76181697847b825a7d1c72e064
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-08 14:52:46 +0200

    Update UPDATE.sh

--- UPDATES/2.0.6/UPDATE.sh
+++ UPDATES/2.0.6/UPDATE.sh
@@ -3,3 +3,15 @@
 # Error: bad username; while reading /etc/cron.d/openpanel (*system*openpanel) ERROR (Syntax error, this crontab file will be ignored)
 sed -i 's/^0 0 \* \* 0 \* root/0 0 * * 0 root/' /etc/cron.d/openpanel
 systemctl restart cron
+
+
+
+FILE="/etc/openpanel/openadmin/service/openadmin.service"
+
+for line in "TimeoutStopSec=30" "KillMode=mixed"; do
+    if ! grep -qxF "$line" "$FILE"; then
+        sed -i "/^Restart=always$/a $line" "$FILE"
+    fi
+done
+
+systemctl daemon-reload
