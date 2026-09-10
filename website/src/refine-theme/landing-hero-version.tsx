395cf2e30d68d5b6d8f83ace7d57f29bddd85e9d
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-10 10:40:25 +0200

    Update landing-hero-version.tsx

--- website/src/refine-theme/landing-hero-version.tsx
+++ website/src/refine-theme/landing-hero-version.tsx
@@ -3,7 +3,7 @@ import { OpenPanelLogoIcon } from './icons/small-openpanel-logo';
 import { HeroBadge } from './hero-badge';
 
 export const LandingHeroVersion = () => {
-    const [version, setVersion] = useState('2.0.4');
+    const [version, setVersion] = useState('2.0.6');
 
     useEffect(() => {
         const fetchVersion = async () => {
