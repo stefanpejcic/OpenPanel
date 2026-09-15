e1f22ef5f39e03a1c6f0755acb859aa8ab114b49
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-15 17:06:30 +0200

    Add UPDATE.sh script for downloading utils.sh

---
+++ UPDATES/2.0.9/UPDATE.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/stefanpejcic/openpanel/issues/1131
+wget -O /usr/local/mail/openmail/openpanel/utils.sh https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/openpanel/utils.sh
+chmod + /usr/local/mail/openmail/openpanel/utils.sh
