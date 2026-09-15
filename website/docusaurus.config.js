c9ab18660ad9903e35f40b6b69d65c2f2108e319
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-15 15:39:40 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,7 +60,7 @@ const siteConfig = {
                               process.env.DISABLE_VERSIONING === "true",
                           versions: {
                               current: {
-                                  label: "2.0.7",
+                                  label: "2.0.8",
                               },
                           },
                           lastVersion: "current",
