36e18d165781648269e995c6348103e7f37adcf9
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-17 10:37:08 +0200

    Update UPDATE.sh

--- UPDATES/2.0.9/UPDATE.sh
+++ UPDATES/2.0.9/UPDATE.sh
@@ -3,3 +3,6 @@
 # https://github.com/stefanpejcic/openpanel/issues/1131
 wget -O /usr/local/mail/openmail/openpanel/utils.sh https://raw.githubusercontent.com/stefanpejcic/OpenMail/refs/heads/main/openpanel/utils.sh
 chmod + /usr/local/mail/openmail/openpanel/utils.sh
+
+
+opencli sentinel
