0dc8929dcd1da891d2117cc7becfc9c18e15bbb4
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-22 18:36:25 +0200

    Create UPDATE.sh

---
+++ UPDATES/2.0.11/UPDATE.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# template for n8n
+wget -O /etc/openpanel/docker/compose/n8n.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/n8n.yml
