35f97a00a67639aa45e858e3f721d088134a77ef
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-23 16:27:25 +0200

    Update UPDATE.sh

--- UPDATES/2.0.11/UPDATE.sh
+++ UPDATES/2.0.11/UPDATE.sh
@@ -2,3 +2,14 @@
 
 # template for n8n
 wget -O /etc/openpanel/docker/compose/n8n.yml https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/docker/compose/n8n.yml
+
+
+# menu_style=classic for existing and menu_style=modern for new installs
+CONFIG_FILE="/etc/openpanel/openpanel/conf/openpanel.config"
+if grep -q "^menu_style" "$CONFIG_FILE"; then
+    :
+else
+    echo "Inserting menu_style=classic into [PANEL]..."
+    sed -i '/^\[PANEL\]/a menu_style=classic' "$CONFIG_FILE"
+    echo "menu_style=classic added successfully."
+fi
